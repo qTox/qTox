01089df91dc7e234a1957f6188e416072b99d5df
retux <github@retux.de>
2014-07-03 14:07:39 +0200

    use superdir libs instead of lib

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,4 +1,4 @@
-@mkdir lib
-%~dp0tools\wget --no-check-certificate http://jenkins.libtoxcore.so/job/libtoxcore-win32-i686/lastSuccessfulBuild/artifact/libtoxcore-win32-i686.zip -O %~dp0lib\libtoxcore-latest.zip
-%~dp0tools\unzip %~dp0lib\libtoxcore-latest.zip -d %~dp0lib\
-@del %~dp0lib\libtoxcore-latest.zip
+@mkdir %~dp0libs
+%~dp0tools\wget --no-check-certificate http://jenkins.libtoxcore.so/job/libtoxcore-win32-i686/lastSuccessfulBuild/artifact/libtoxcore-win32-i686.zip -O %~dp0libs\libtoxcore-latest.zip
+%~dp0tools\unzip %~dp0libs\libtoxcore-latest.zip -d %~dp0libs\
+@del %~dp0libs\libtoxcore-latest.zip
