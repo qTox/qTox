6a27039fb2e18efa7cf883f93f6994e4261e70d5
Сухарик <suhr@i2pmail.org>
2014-07-04 20:36:43 +0400

    translations/ru: minor improvements

--- translations/ru.ts
+++ translations/ru.ts
@@ -4,82 +4,82 @@
 <context>
     <name>AddFriendForm</name>
     <message>
-        <location filename="widget/form/addfriendform.cpp" line="15"/>
+        <location filename="../widget/form/addfriendform.cpp" line="15"/>
         <source>Add Friends</source>
         <translation>Добавление друзей</translation>
     </message>
     <message>
-        <location filename="widget/form/addfriendform.cpp" line="18"/>
+        <location filename="../widget/form/addfriendform.cpp" line="18"/>
         <source>Tox ID</source>
         <comment>Tox ID of the person you&apos;re sending a friend request to</comment>
         <translation>Tox ID</translation>
     </message>
     <message>
-        <location filename="widget/form/addfriendform.cpp" line="19"/>
+        <location filename="../widget/form/addfriendform.cpp" line="19"/>
         <source>Message</source>
         <comment>The message you send in friend requests</comment>
         <translation>Сообщение</translation>
     </message>
     <message>
-        <location filename="widget/form/addfriendform.cpp" line="20"/>
+        <location filename="../widget/form/addfriendform.cpp" line="20"/>
         <source>Send friend request</source>
         <translatorcomment>Мне не нравится, но другого не придумал, и фейсбук использует это</translatorcomment>
         <translation>Отправить запрос на добавление в друзья</translation>
     </message>
     <message>
-        <location filename="widget/form/addfriendform.cpp" line="67"/>
+        <location filename="../widget/form/addfriendform.cpp" line="67"/>
         <source>Tox me maybe?</source>
         <comment>Default message in friend requests if the field is left blank. Write something appropriate!</comment>
         <translatorcomment>Вот таким нехитрым и незамысловатым образом решаются сложные переводчиские проблемы</translatorcomment>
         <translation>Добавь меня, а?</translation>
     </message>
     <message>
-        <location filename="widget/form/addfriendform.cpp" line="75"/>
+        <location filename="../widget/form/addfriendform.cpp" line="75"/>
         <source>Please fill in a valid Tox ID</source>
         <comment>Tox ID of the friend you&apos;re sending a friend request to</comment>
         <translation>Пожалуйста, введите корректный Tox ID</translation>
     </message>
     <message>
-        <location filename="widget/form/addfriendform.cpp" line="90"/>
+        <location filename="../widget/form/addfriendform.cpp" line="92"/>
         <source>Error while looking up DNS</source>
         <comment>The DNS gives the Tox ID associated to toxme.se addresses</comment>
         <translation>Ошибка при просмотре DNS</translation>
     </message>
     <message>
-        <location filename="widget/form/addfriendform.cpp" line="96"/>
+        <location filename="../widget/form/addfriendform.cpp" line="98"/>
         <source>Unexpected number of text records</source>
         <comment>Error with the DNS</comment>
-        <translation type="unfinished"></translation>
+        <translation>Непредвиденное количество текстовых записей</translation>
     </message>
     <message>
-        <location filename="widget/form/addfriendform.cpp" line="102"/>
+        <location filename="../widget/form/addfriendform.cpp" line="104"/>
         <source>Unexpected number of values in text record</source>
         <comment>Error with the DNS</comment>
-        <translation type="unfinished"></translation>
+        <translation>Непредвиденное количество значений в текстовой записи</translation>
     </message>
     <message>
-        <location filename="widget/form/addfriendform.cpp" line="109"/>
+        <location filename="../widget/form/addfriendform.cpp" line="111"/>
         <source>The DNS lookup does not contain any Tox ID</source>
         <comment>Error with the DNS</comment>
         <translation>В ответе DNS ни одного Tox ID</translation>
     </message>
     <message>
-        <location filename="widget/form/addfriendform.cpp" line="115"/>
-        <location filename="widget/form/addfriendform.cpp" line="121"/>
+        <location filename="../widget/form/addfriendform.cpp" line="117"/>
+        <location filename="../widget/form/addfriendform.cpp" line="123"/>
         <source>The DNS lookup does not contain a valid Tox ID</source>
         <comment>Error with the DNS</comment>
-        <translation type="unfinished">Ответ DNS не содержит корректных Tox ID</translation>
+        <translation>Ответ DNS не содержит корректных Tox ID</translation>
     </message>
 </context>
 <context>
     <name>Camera</name>
     <message>
-        <location filename="widget/camera.cpp" line="145"/>
+        <location filename="../widget/camera.cpp" line="145"/>
         <source>Camera eror</source>
         <translation>Ошибка камеры</translation>
     </message>
     <message>
-        <location filename="widget/camera.cpp" line="146"/>
+        <location filename="../widget/camera.cpp" line="146"/>
         <source>Camera format %1 not supported, can&apos;t use the camera</source>
         <translation>Формат камеры %1 не поддерживается, невозможно использовать камеру</translation>
     </message>
@@ -87,13 +87,13 @@
 <context>
     <name>ChatForm</name>
     <message>
-        <location filename="widget/form/chatform.cpp" line="261"/>
+        <location filename="../widget/form/chatform.cpp" line="261"/>
         <source>Send a file</source>
-        <translation>Оправить файл</translation>
+        <translation type="unfinished">Отправить файл</translation>
     </message>
     <message>
-        <location filename="widget/form/chatform.cpp" line="586"/>
-        <location filename="widget/form/chatform.cpp" line="592"/>
+        <location filename="../widget/form/chatform.cpp" line="586"/>
+        <location filename="../widget/form/chatform.cpp" line="592"/>
         <source>Save chat log</source>
         <translation>Сохранить лог чата</translation>
     </message>
@@ -101,7 +101,7 @@
 <context>
     <name>CopyableElideLabel</name>
     <message>
-        <location filename="widget/tool/copyableelidelabel.cpp" line="29"/>
+        <location filename="../widget/tool/copyableelidelabel.cpp" line="29"/>
         <source>Copy</source>
         <translation>Копировать</translation>
     </message>
@@ -109,7 +109,7 @@
 <context>
     <name>FileTransfertWidget</name>
     <message>
-        <location filename="widget/filetransfertwidget.cpp" line="231"/>
+        <location filename="../widget/filetransfertwidget.cpp" line="233"/>
         <source>Save a file</source>
         <comment>Title of the file saving dialog</comment>
         <translation>Сохранить файл</translation>
@@ -118,36 +118,36 @@
 <context>
     <name>FriendRequestDialog</name>
     <message>
-        <location filename="widget/tool/friendrequestdialog.cpp" line="30"/>
+        <location filename="../widget/tool/friendrequestdialog.cpp" line="30"/>
         <source>Friend request</source>
         <comment>Title of the window to aceept/deny a friend request</comment>
         <translatorcomment>Мне не нравится, но другого не придумал, и фейсбук использует это</translatorcomment>
         <translation>Запрос на добавление в друзья</translation>
     </message>
     <message>
-        <location filename="widget/tool/friendrequestdialog.cpp" line="32"/>
+        <location filename="../widget/tool/friendrequestdialog.cpp" line="32"/>
         <source>Someone wants to make friends with you</source>
         <translatorcomment>«Подружиться», вероятно, недостаточно вежливо</translatorcomment>
         <translation>Кто-то хочет добавить вас в друзья</translation>
     </message>
     <message>
-        <location filename="widget/tool/friendrequestdialog.cpp" line="33"/>
+        <location filename="../widget/tool/friendrequestdialog.cpp" line="33"/>
         <source>User ID:</source>
         <translation>ID пользователя:</translation>
     </message>
     <message>
-        <location filename="widget/tool/friendrequestdialog.cpp" line="37"/>
+        <location filename="../widget/tool/friendrequestdialog.cpp" line="37"/>
         <source>Friend request message:</source>
         <translation>Текст запроса:</translation>
     </message>
     <message>
-        <location filename="widget/tool/friendrequestdialog.cpp" line="44"/>
+        <location filename="../widget/tool/friendrequestdialog.cpp" line="44"/>
         <source>Accept</source>
         <comment>Accept a friend request</comment>
         <translation>Принять</translation>
     </message>
     <message>
-        <location filename="widget/tool/friendrequestdialog.cpp" line="45"/>
+        <location filename="../widget/tool/friendrequestdialog.cpp" line="45"/>
         <source>Reject</source>
         <comment>Reject a friend request</comment>
         <translation>Отклонить</translation>
@@ -156,19 +156,19 @@
 <context>
     <name>FriendWidget</name>
     <message>
-        <location filename="widget/friendwidget.cpp" line="65"/>
+        <location filename="../widget/friendwidget.cpp" line="71"/>
         <source>Copy friend ID</source>
         <comment>Menu to copy the Tox ID of that friend</comment>
         <translation>Копировать ID друга</translation>
     </message>
     <message>
-        <location filename="widget/friendwidget.cpp" line="66"/>
+        <location filename="../widget/friendwidget.cpp" line="72"/>
         <source>Invite in group</source>
         <comment>Menu to invite a friend in a groupchat</comment>
         <translation>Пригласить в группу</translation>
     </message>
     <message>
-        <location filename="widget/friendwidget.cpp" line="76"/>
+        <location filename="../widget/friendwidget.cpp" line="82"/>
         <source>Remove friend</source>
         <comment>Menu to remove the friend from our friendlist</comment>
         <translation>Удалить друга</translation>
@@ -177,17 +177,23 @@
 <context>
     <name>GroupChatForm</name>
     <message>
-        <location filename="widget/form/groupchatform.cpp" line="155"/>
+        <location filename="../widget/form/groupchatform.cpp" line="32"/>
+        <source>%1 users in chat</source>
+        <comment>Number of users in chat</comment>
+        <translation>%1 пользователей в чате</translation>
+    </message>
+    <message>
+        <location filename="../widget/form/groupchatform.cpp" line="155"/>
         <source>&lt;Unknown&gt;</source>
         <translation>&lt;Неизвестно&gt;</translation>
     </message>
     <message>
-        <location filename="widget/form/groupchatform.cpp" line="220"/>
+        <location filename="../widget/form/groupchatform.cpp" line="220"/>
         <source>%1 users in chat</source>
         <translation>%1 пользователей в чате</translation>
     </message>
     <message>
-        <location filename="widget/form/groupchatform.cpp" line="239"/>
+        <location filename="../widget/form/groupchatform.cpp" line="239"/>
         <source>Save chat log</source>
         <translation>Сохранить лог чата</translation>
     </message>
@@ -195,50 +201,61 @@
 <context>
     <name>GroupWidget</name>
     <message>
-        <location filename="widget/groupwidget.cpp" line="68"/>
+        <location filename="../widget/groupwidget.cpp" line="73"/>
         <source>Quit group</source>
         <comment>Menu to quit a groupchat</comment>
         <translation>Покинуть группу</translation>
     </message>
     <message>
-        <location filename="widget/groupwidget.cpp" line="126"/>
+        <location filename="../widget/groupwidget.cpp" line="38"/>
+        <location filename="../widget/groupwidget.cpp" line="128"/>
         <source>%1 users in chat</source>
         <translation>%1 пользователей в чате</translation>
     </message>
     <message>
-        <location filename="widget/groupwidget.cpp" line="128"/>
+        <location filename="../widget/groupwidget.cpp" line="40"/>
+        <location filename="../widget/groupwidget.cpp" line="130"/>
         <source>0 users in chat</source>
         <translation>Ни одного пользователя в чате</translation>
     </message>
 </context>
 <context>
+    <name>SelfCamView</name>
+    <message>
+        <location filename="../widget/selfcamview.cpp" line="16"/>
+        <source>Tox video test</source>
+        <comment>Title of the window to test the video/webcam</comment>
+        <translation>Проверка видео</translation>
+    </message>
+</context>
+<context>
     <name>SettingsForm</name>
     <message>
-        <location filename="widget/form/settingsform.cpp" line="13"/>
+        <location filename="../widget/form/settingsform.cpp" line="13"/>
         <source>User Settings</source>
         <comment>&quot;Headline&quot; of the window</comment>
         <translation>Пользовательские настройки</translation>
     </message>
     <message>
-        <location filename="widget/form/settingsform.cpp" line="16"/>
+        <location filename="../widget/form/settingsform.cpp" line="16"/>
         <source>Name</source>
         <comment>Username/nick</comment>
         <translation>Имя</translation>
     </message>
     <message>
-        <location filename="widget/form/settingsform.cpp" line="17"/>
+        <location filename="../widget/form/settingsform.cpp" line="17"/>
         <source>Status</source>
         <comment>Status message</comment>
         <translation>Статус</translation>
     </message>
     <message>
-        <location filename="widget/form/settingsform.cpp" line="22"/>
+        <location filename="../widget/form/settingsform.cpp" line="22"/>
         <source>Test video</source>
         <comment>Text on a button to test the video/webcam</comment>
         <translation>Проверить видео</translation>
     </message>
     <message>
-        <location filename="widget/form/settingsform.cpp" line="23"/>
+        <location filename="../widget/form/settingsform.cpp" line="23"/>
         <source>Enable IPv6 (recommended)</source>
         <comment>Text on a checkbox to enable IPv6</comment>
         <translation>Включить IPv6 (рекомендуется)</translation>
@@ -247,44 +264,45 @@
 <context>
     <name>Widget</name>
     <message>
-        <location filename="widget.ui" line="20"/>
+        <location filename="../widget.ui" line="26"/>
         <source>Tox</source>
         <translation>Tox</translation>
     </message>
     <message>
-        <location filename="widget.ui" line="1593"/>
+        <location filename="../widget.ui" line="1683"/>
         <source>Your name</source>
         <translation>Ваше имя</translation>
     </message>
     <message>
-        <location filename="widget.ui" line="1675"/>
+        <location filename="../widget.ui" line="1759"/>
         <source>Your status</source>
         <translation>Ваш статус</translation>
     </message>
     <message>
-        <location filename="widget.ui" line="2811"/>
+        <location filename="../widget.ui" line="3005"/>
         <source>Close</source>
         <translation>Закрыть</translation>
     </message>
     <message>
-        <location filename="widget.ui" line="2814"/>
+        <location filename="../widget.ui" line="3008"/>
         <source>Ctrl+Q</source>
         <translation>Ctrl+Q</translation>
     </message>
     <message>
-        <location filename="widget/widget.cpp" line="1112"/>
+        <location filename="../widget/widget.cpp" line="1149"/>
         <source>Online</source>
         <comment>Button to set your status to &apos;Online&apos;</comment>
         <translation>В сети</translation>
     </message>
     <message>
-        <location filename="widget/widget.cpp" line="1113"/>
+        <location filename="../widget/widget.cpp" line="1150"/>
         <source>Away</source>
         <comment>Button to set your status to &apos;Away&apos;</comment>
-        <translation>Ушёл</translation>
+        <translatorcomment>Вероятно, это не столь долгое путешествие</translatorcomment>
+        <translation>Отошёл</translation>
     </message>
     <message>
-        <location filename="widget/widget.cpp" line="1114"/>
+        <location filename="../widget/widget.cpp" line="1151"/>
         <source>Busy</source>
         <comment>Button to set your status to &apos;Busy&apos;</comment>
         <translation>Занят</translation>
