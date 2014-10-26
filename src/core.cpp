cb812005824f3bd8222ecebd156421b78c3f00fd
Tux3 / Mlkj / !Lev.uXFMLA <barrdetwix@gmail.com>
2014-10-26 21:33:24 +0100

    Fix leak, enable NRVO in Core::getStatusMessage

--- src/core.cpp
+++ src/core.cpp
@@ -1016,13 +1016,13 @@ QString Core::getIDString()
 
 QString Core::getStatusMessage()
 {
+    QString sname;
     int size = tox_get_self_status_message_size(tox);
     uint8_t* name = new uint8_t[size];
     if (tox_get_self_status_message(tox, name, size) == size)
-        return QString(CString::toString(name, size));
-    else
-        return QString();
+        sname = CString::toString(name, size);
     delete[] name;
+    return sname;
 }
 
 void Core::setStatusMessage(const QString& message)
