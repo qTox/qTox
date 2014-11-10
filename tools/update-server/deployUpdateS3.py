96a05096108a379f0dc314f43416192b5b3e4ba5
Tux3 / Mlkj / !Lev.uXFMLA <barrdetwix@gmail.com>
2014-11-10 13:49:34 +0100

    Update S3 update deploy script

--- tools/update-server/deployUpdateS3.py
+++ tools/update-server/deployUpdateS3.py
@@ -20,9 +20,11 @@ prefix = "/qtox/win32/" # We only support Windows for now
 conn = boto.connect_s3()
 bucket = conn.get_bucket('qtox-updater')
 print("Connected to S3")
+sys.stdout.flush()
 
 # Delete the old version, so nobody downloads a half-uploaded update
 print("Deleting version ...")
+sys.stdout.flush()
 oldversion = Key(bucket)
 oldversion.key = prefix+'version'
 bucket.delete_key(oldversion)
@@ -31,6 +33,7 @@ bucket.delete_key(oldversion)
 filenames = next(os.walk(localpath+prefix+'/files/'))[2]
 for filename in filenames:
 	print("Uploading files/"+filename+'...')
+	sys.stdout.flush()
 	k = Key(bucket)
 	k.key = prefix+'files/'+filename
 	k.set_contents_from_filename(localpath+prefix+'/files/'+filename)
@@ -38,12 +41,14 @@ for filename in filenames:
 
 # Upload version and flist
 print("Uploading flist...")
+sys.stdout.flush()
 flist = Key(bucket)
 flist.key = prefix+'flist'
 flist.set_contents_from_filename(localpath+prefix+'flist')
 flist.make_public()
 
 print("Uploading version...")
+sys.stdout.flush()
 version = Key(bucket)
 version.key = prefix+'version'
 version.set_contents_from_filename(localpath+prefix+'version')
