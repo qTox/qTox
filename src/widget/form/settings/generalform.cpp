f2cc4867440d298b5cdf290c96eb97e22a247bb3
dubslow <bunslow@gmail.com>
2014-11-10 02:43:07 -0600

    UI things

--- src/widget/form/settings/generalform.cpp
+++ src/widget/form/settings/generalform.cpp
@@ -44,6 +44,7 @@ GeneralForm::GeneralForm(SettingsWidget *myParent) :
 
     bodyUI->checkUpdates->setVisible(AUTOUPDATE_ENABLED);
     bodyUI->checkUpdates->setChecked(Settings::getInstance().getCheckUpdates());
+    bodyUI->trayLayout->addStretch();
     
     bodyUI->cbEnableIPv6->setChecked(Settings::getInstance().getEnableIPv6());
     for (int i = 0; i < langs.size(); i++)
@@ -298,3 +299,4 @@ void GeneralForm::onSetShowInFront()
 {
    Settings::getInstance().setShowInFront(bodyUI->showInFront->isChecked());
 }
+
