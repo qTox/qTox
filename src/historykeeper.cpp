e782a9d5876bdfdeb9e6b1cf9722080d6b813017
apprb <apprb@users.noreply.github.com>
2014-11-13 11:30:29 +0900

    batch processing

--- src/historykeeper.cpp
+++ src/historykeeper.cpp
@@ -147,10 +147,12 @@ int HistoryKeeper::addChatEntry(const QString& chat, const QString& message, con
     int chat_id = getChatID(chat, ctSingle).first;
     int sender_id = getAliasID(sender);
 
+    db->exec("BEGIN TRANSACTION");
     db->exec(QString("INSERT INTO history (timestamp, chat_id, sender, message)") +
              QString("VALUES (%1, %2, %3, '%4');")
              .arg(dt.toMSecsSinceEpoch()).arg(chat_id).arg(sender_id).arg(wrapMessage(message)));
     db->exec(QString("INSERT INTO sent_status (status) VALUES (%1)").arg(isSent));
+    db->exec("COMMIT TRANSACTION");
 
     messageID++;
     return messageID;
