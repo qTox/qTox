3bc6909c96f01e3e5b0cdd2f4a9a98fa03ab2e95
Tux3 / Mlkj / !Lev.uXFMLA <barrdetwix@gmail.com>
2014-11-14 18:14:33 +0100

    Apply valgrind fix to 1on1 audio too
    I haven't seen valgrind complain too much, but let's fix it preemptively

--- src/coreav.cpp
+++ src/coreav.cpp
@@ -231,6 +231,7 @@ void Core::sendCallAudio(int callId, ToxAv* toxav)
     alcGetIntegerv(alInDev, ALC_CAPTURE_SAMPLES, sizeof(samples), &samples);
     if(samples >= framesize)
     {
+        memset(buf, 0, framesize*2); // Avoid uninitialized values (Valgrind)
         alcCaptureSamples(alInDev, buf, framesize);
         frame = 1;
     }
