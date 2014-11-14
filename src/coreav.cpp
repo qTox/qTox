7c6f233d537613f56ef9d9b969242c83ded02cf3
Tux3 / Mlkj / !Lev.uXFMLA <barrdetwix@gmail.com>
2014-11-14 21:16:28 +0100

    Remove debug statements

--- src/coreav.cpp
+++ src/coreav.cpp
@@ -625,17 +625,11 @@ void Core::joinGroupCall(int groupId)
     Core* core = Core::getInstance();
     ToxAv* toxav = core->toxav;
 
-    qWarning()<<"Core is "<<(long)core<<", toxav is "<<(long)toxav;
-
     groupCalls[groupId].sendAudioTimer = new QTimer();
     groupCalls[groupId].active = true;
     groupCalls[groupId].sendAudioTimer->setInterval(5);
     groupCalls[groupId].sendAudioTimer->setSingleShot(true);
-    connect(groupCalls[groupId].sendAudioTimer, &QTimer::timeout, [=]()
-    {
-        qWarning()<<"In the lambda toxav is "<<(long)toxav;
-        sendGroupCallAudio(groupId,toxav);}
-    );
+    connect(groupCalls[groupId].sendAudioTimer, &QTimer::timeout, [=](){sendGroupCallAudio(groupId,toxav);});
     groupCalls[groupId].sendAudioTimer->start();
 }
 
