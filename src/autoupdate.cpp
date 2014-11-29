5d297a78d51fb559ef86c2098f30296c7934ec05
Zetok Zalbavar <zexavexxe@gmail.com>
2014-11-29 11:27:36 +0000

    Sort res.qrc and remove some unnecessary spaces

--- src/autoupdate.cpp
+++ src/autoupdate.cpp
@@ -428,7 +428,7 @@ void AutoUpdater::checkUpdatesAsyncInteractiveWorker()
         return;
 
     if (Widget::getInstance()->askMsgboxQuestion(QObject::tr("Update", "The title of a message box"),
-        QObject::tr("An update is available, do you want to download it now ?\nIt will be installed when qTox restarts.")))
+        QObject::tr("An update is available, do you want to download it now?\nIt will be installed when qTox restarts.")))
     {
         downloadUpdate();
     }
