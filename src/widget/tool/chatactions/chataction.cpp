84dea5e12cda03335b868f4930dc9d33e83a60d8
Dubslow <bunslow@gmail.com>
2014-12-13 12:18:11 -0600

    Revert "fix #902" @apprb #932 This reverts commit 13306031e238258a3faf2909a01987cf093765dc.

--- src/widget/tool/chatactions/chataction.cpp
+++ src/widget/tool/chatactions/chataction.cpp
@@ -25,7 +25,7 @@ QTextBlockFormat ChatAction::nameFormat, ChatAction::dateFormat;
 
 QString ChatAction::toHtmlChars(const QString &str)
 {
-    static QList<QPair<QString, QString>> replaceList = {{"&","&amp;"}, {">","&gt;"}, {"<","&lt;"}, {" ", "&nbsp;"}}; // {"&","&amp;"} should be always first
+    static QList<QPair<QString, QString>> replaceList = {{"&","&amp;"}, {">","&gt;"}, {"<","&lt;"}};
     QString res = str;
 
     for (auto &it : replaceList)
