5cbe175ddc490a6b2b2b68288e30913cfa7f4cb0
Dubslow <bunslow@gmail.com>
2014-12-16 18:50:28 -0600

    update the build system

--- install_libfilteraudio.sh
+++ install_libfilteraudio.sh
@@ -1,31 +1,40 @@
 #!/bin/sh
-CURRENT_DIR=$DIRSTACK
-SOURCE_DIR="filter_audio/"
-SOURCE_PATH="./../"
-LIB_DIR="/usr/local/lib/"
-INCLUDE_DIR="/usr/local/include/"
 
-echo "Clone filter_audio from GitHub.com"
-cd $SOURCE_PATH
-git clone --quiet https://github.com/irungentoo/filter_audio.git $SOURCE_DIR
+if [ -z $1 ]; then
+  SOURCE_DIR="filter_audio/"
+else
+  SOURCE_DIR="$1/"
+fi
 
-echo "Compile filter_audio"
+if [ -z "$2" ]; then
+  LIB_DIR="/usr/local/lib/"
+  INCLUDE_DIR="/usr/local/include/"
+else
+  LIB_DIR="$2/lib/"
+  INCLUDE_DIR="$2/include/"
+fi
+
+echo "Cloning filter_audio from GitHub.com"
+git clone https://github.com/irungentoo/filter_audio.git $SOURCE_DIR
+
+echo "Compiling filter_audio"
 cd $SOURCE_DIR
 gcc -c -fPIC filter_audio.c aec/*.c agc/*.c ns/*.c other/*.c  -lm -lpthread
 
-echo "Create shared object file"
+echo "Creating shared object file"
 gcc *.o -shared -o libfilteraudio.so
 
-echo "Clean up"
+echo "Cleaning up"
 rm *.o
 
-echo "Install libfilteraudio.so"
-sudo cp libfilteraudio.so $LIB_DIR
+muhcmd="cp libfilteraudio.so $LIB_DIR"
+[ -z "$2" ] && muhcmd="sudo $muhcmd"
+echo "Installing libfilteraudio.so with $muhcmd"
+$muhcmd
 
-echo "Install include files"
-sudo cp *.h $INCLUDE_DIR
+muhcmd="cp *.h $INCLUDE_DIR"
+[ -z "$2" ] && muhcmd="sudo $muhcmd"
+echo "Installing include files with $muhcmd"
+$muhcmd
 
 echo "Finished."
-cd $CURRENT_DIR
-exit 1
-
