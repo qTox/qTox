0af05929f723e15ab540c4276a665a68dba1b724
krepa098 <krepa098@users.noreply.github.com>
2015-02-03 16:15:51 +0100

    fixup! Merge branch 'master' into chatlog_v3_1

--- src/widget/widget.cpp
+++ src/widget/widget.cpp
@@ -696,9 +696,6 @@ void Widget::addFriend(int friendId, const QString &userId)
     QLayout* layout = contactListWidget->getFriendLayout(Status::Offline);
     layout->addWidget(newfriend->getFriendWidget());
 
-    if (Settings::getInstance().getEnableLogging())
-        newfriend->getChatForm()->loadHistory(QDateTime::currentDateTime().addDays(-7), true);
-
     connect(settingsWidget, &SettingsWidget::compactToggled, newfriend->getFriendWidget(), &GenericChatroomWidget::onCompactChanged);
     connect(newfriend->getFriendWidget(), SIGNAL(chatroomWidgetClicked(GenericChatroomWidget*)), this, SLOT(onChatroomWidgetClicked(GenericChatroomWidget*)));
     connect(newfriend->getFriendWidget(), SIGNAL(removeFriend(int)), this, SLOT(removeFriend(int)));
