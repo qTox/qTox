c37e6e4dd9a45fc95c4eb9667712cac533ca7432
Stefan Merettig <stefan-merettig@nuriaproject.org>
2015-04-02 16:29:27 +0200

    Screen grabber: Only show flyout if the attach button is enabeld
    Fixes the bug that the flyout was shown in group chats.

--- src/widget/form/genericchatform.cpp
+++ src/widget/form/genericchatform.cpp
@@ -423,6 +423,9 @@ bool GenericChatForm::eventFilter(QObject* object, QEvent* event)
     if (object != this->fileButton && object != this->fileFlyout)
         return false;
     
+    if (!qobject_cast<QWidget*>(object)->isEnabled())
+        return false;
+    
     switch(event->type())
     {
     case QEvent::Enter:
