66314bc38dcb6fa9b68d3363c6cf015b1c97b9d2
tux3 <barrdetwix@gmail.com>
2015-04-24 15:38:21 +0200

    Fix nullptr dereference in IPC::isCurrentOwner
    It would only trigger when multiple instances where running in parallel,
    with one having enough privilege to block the other from accessing the shared memory (e.g. root)

--- src/ipc.cpp
+++ src/ipc.cpp
@@ -134,8 +134,14 @@ bool IPC::isCurrentOwner()
 {
     if (globalMemory.lock())
     {
-        /// TODO: Segfault on exit on "mov rdx,QWORD PTR [rax]" w/ rax=0.
-        bool isOwner = ((*(uint64_t*)globalMemory.data()) == globalId);
+        void* data = globalMemory.data();
+        if (!data)
+        {
+            qWarning() << "IPC: isCurrentOwner failed to access the memory, returning false";
+            globalMemory.unlock();
+            return false;
+        }
+        bool isOwner = ((*(uint64_t*)data) == globalId);
         globalMemory.unlock();
         return isOwner;
     }
