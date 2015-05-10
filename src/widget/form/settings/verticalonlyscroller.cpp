6c8b130a25c4ac3dbb0e250655d50e126d141091
Ilya Kowalewski <illya.kovalevskyy@gmail.com>
2015-05-11 00:40:08 +0300

    Redesign draft for settings pane and proper UX for Mac

---
+++ src/widget/form/settings/verticalonlyscroller.cpp
@@ -0,0 +1,31 @@
+/*
+    Copyright (C) 2014 by Project Tox <https://tox.im>
+
+    This file is part of qTox, a Qt-based graphical interface for Tox.
+
+    This program is libre software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    See the COPYING file for more details.
+*/
+
+#include <QResizeEvent>
+
+#include "verticalonlyscroller.h"
+
+VerticalOnlyScroller::VerticalOnlyScroller(QWidget *parent) :
+    QScrollArea(parent)
+{
+}
+
+void VerticalOnlyScroller::resizeEvent(QResizeEvent *event)
+{
+    QScrollArea::resizeEvent(event);
+    if (widget())
+        widget()->setMaximumWidth(event->size().width());
+}
---
+++ src/widget/form/settings/verticalonlyscroller.h
@@ -0,0 +1,34 @@
+/*
+    Copyright (C) 2014 by Project Tox <https://tox.im>
+
+    This file is part of qTox, a Qt-based graphical interface for Tox.
+
+    This program is libre software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    See the COPYING file for more details.
+*/
+
+#ifndef VERTICALONLYSCROLLER_H
+#define VERTICALONLYSCROLLER_H
+
+#include <QScrollArea>
+
+class QResizeEvent;
+
+class VerticalOnlyScroller : public QScrollArea
+{
+    Q_OBJECT
+public:
+    explicit VerticalOnlyScroller(QWidget *parent = 0);
+
+protected:
+    void resizeEvent(QResizeEvent *event);
+};
+
+#endif // VERTICALONLYSCROLLER_H
