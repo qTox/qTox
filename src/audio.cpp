1285b5811cb39b757519668f860bd9a310c8f6a1
agilob <info@agilob.net>
2015-05-11 20:32:38 +0100

    this sould be better ;_;

--- src/audio.cpp
+++ src/audio.cpp
@@ -232,10 +232,10 @@ void Audio::playMono16Sound(const QByteArray& data)
     ALuint buffer;
     alGenBuffers(1, &buffer);
     alBufferData(buffer, AL_FORMAT_MONO16, data.data(), data.size(), 44100);
+    alSourcef(alMainSource, AL_GAIN, outputVolume);    
     alSourcei(alMainSource, AL_BUFFER, buffer);
     alSourcePlay(alMainSource);
     alDeleteBuffers(1, &buffer);
-    alSourcef(alMainSource, AL_GAIN, outputVolume);
 }
 
 void Audio::playGroupAudioQueued(Tox*,int group, int peer, const int16_t* data,
