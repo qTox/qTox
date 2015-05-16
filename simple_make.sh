b99c1a6e8fd549deba9fa65aea86e89d7df0ac58
Marcel <github@retux.de>
2015-05-17 00:33:43 +0200

    updated simple_make.sh

--- simple_make.sh
+++ simple_make.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
 if which apt-get; then
-    sudo apt-get install build-essential qt5-qmake qt5-default libopenal-dev libopencv-dev \
-                         libtool autotools-dev automake checkinstall check libopus-dev libvpx-dev \
-			 qttools5-dev-tools qtchooser libxss-dev libqt5svg5* libqrencode-dev \
-			 libqt5opengl5-dev
+    sudo apt-get install \
+        git build-essential qt5-qmake qt5-default qttools5-dev-tools \
+        libqt5opengl5-dev libqt5svg5-dev libopenal-dev libopencv-dev \
+        libxss-dev qrencode libqrencode-dev libtool autotools-dev \
+        automake checkinstall check libopus-dev libvpx-dev libsodium-dev
 elif which pacman; then
-    sudo pacman -S --needed base-devel qt5 opencv openal opus libvpx libxss qt5-svg qrencode
-elif which yum; then
-    sudo yum groupinstall "Development Tools"
-    sudo yum install qt-devel qt-doc qt-creator opencv-devel openal-soft-devel libtool autoconf automake check check-devel libXScrnSaver-devel qt5-qtsvg qrencode
+    sudo pacman -S --needed \
+        git base-devel qt5 opencv openal libxss qrencode opus libvpx \
+        libsodium
+elif which dnf; then
+    sudo dnf group install \
+        "Development Tools"
+    sudo dnf install \
+        git qt-devel qt-doc qt-creator qt5-qtsvg opencv-devel \
+        openal-soft-devel libXScrnSaver-devel qrencode-devel
 else
     echo "Unknown package manager, attempting to compile anyways"
 fi
