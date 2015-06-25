49844509251ee41ea890f1db929627e0fd6a3849
rku <rokups@zoho.com>
2015-06-25 20:41:10 +0300

    Added ffmpeg support to windows/bootstrap.sh

--- windows/bootstrap.sh
+++ windows/bootstrap.sh
@@ -32,7 +32,7 @@ fi
 ## filter_audio
 if [ ! -d $QTOX_DIR/libs/filter_audio ]; then
     git clone https://github.com/irungentoo/filter_audio.git $QTOX_DIR/libs/filter_audio
-	rm bin/libfilteraudio.dll
+   rm bin/libfilteraudio.dll
 else
     pushd $QTOX_DIR/libs/filter_audio
     git pull
@@ -42,31 +42,31 @@ fi
 if [ ! -f "bin/libfilteraudio.dll" ]; then
     pushd $QTOX_DIR/libs/filter_audio
     PREFIX="$QTOX_DIR/libs" CC="gcc.exe" make install
-	mv libfilteraudio.dll.a $QTOX_DIR/libs/lib
+   mv libfilteraudio.dll.a $QTOX_DIR/libs/lib
     popd
-	if [ -f "lib/libfilteraudio.dll" ]; then
-		mv lib/libfilteraudio.dll bin/
-	fi
+   if [ -f "lib/libfilteraudio.dll" ]; then
+      mv lib/libfilteraudio.dll bin/
+   fi
 fi
 
 
 ## qrencode
 if [ ! -f "qrencode-3.4.4.tar.gz" ]; then
-	wget http://fukuchi.org/works/qrencode/qrencode-3.4.4.tar.gz
+   wget http://fukuchi.org/works/qrencode/qrencode-3.4.4.tar.gz
 fi
 
 if [ ! -d "$QTOX_DIR/libs/qrencode-3.4.4" ]; then
-	tar -xvf qrencode-3.4.4.tar.gz
-	rm -rf lib/qrcodelib.dll
+   tar -xvf qrencode-3.4.4.tar.gz
+   rm -rf lib/qrcodelib.dll
 fi
 
 if [ ! -f "lib/libqrencode.a" ]; then
     pushd $QTOX_DIR/libs/qrencode-3.4.4
-	./autogen.sh
-	./configure --prefix=$QTOX_DIR/libs --without-tests --without-libiconv-prefix --without-tools
-	make
-	make install
-	popd
+   ./autogen.sh
+   ./configure --prefix=$QTOX_DIR/libs --without-tests --without-libiconv-prefix --without-tools
+   make
+   make install
+   popd
 fi
 
 
@@ -139,6 +139,32 @@ if [ ! -f "bin/libopencv_core249.dll" ]; then
            rm -rf $QTOX_DIR/libs/$arch
        fi
    done
+   popd
+fi
+
+## ffmpeg
+if [ ! -f "ffmpeg-2.7.tar.bz2" ]; then
+   wget http://ffmpeg.org/releases/ffmpeg-2.7.tar.bz2 -O ffmpeg-2.7.tar.bz2
+   rm -rf ffmpeg-2.7
+fi
+
+if [ ! -d "ffmpeg-2.7" ]; then
+   tar -xvf ffmpeg-2.7.tar.bz2
+   rm bin/avcodec-56.dll
+   pushd ffmpeg-2.7
+   patch -p1 < $QTOX_DIR/windows/ffmpeg-2.7-mingw.diff
+   popd
+fi
+
+if [ ! -f "bin/avcodec-56.dll" ]; then
+   pushd ffmpeg-2.7
+   ./configure --target-os=mingw32 --prefix=$QTOX_DIR/libs \
+      --enable-memalign-hack --disable-swscale-alpha --disable-programs --disable-doc --disable-postproc \
+      --disable-avfilter --disable-avresample --disable-swresample --disable-protocols --disable-filters \
+      --disable-network --disable-muxers --disable-sdl --disable-iconv --disable-bzlib --disable-lzma \
+      --disable-zlib --disable-xlib --disable-encoders --enable-shared --disable-static --disable-yasm
+   make
+   make install
    popd
 fi
 
