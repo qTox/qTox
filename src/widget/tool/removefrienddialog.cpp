3d72a75ed3cd1a8800d8c33dbd27fc8307bdbddd
Impyy <alex@impy.me>
2015-09-23 17:06:57 +0200

    Made the 'remove friend dialog' userfriendly

--- src/widget/tool/removefrienddialog.cpp
+++ src/widget/tool/removefrienddialog.cpp
@@ -9,10 +9,7 @@ RemoveFriendDialog::RemoveFriendDialog(QWidget *parent, const Friend *f)
     setAttribute(Qt::WA_QuitOnClose, false);
     ui.setupUi(this);
     ui.label->setText(ui.label->text().replace("&lt;name&gt;", f->getDisplayedName()));
-    auto removeButton = ui.buttonBox->button(QDialogButtonBox::Ok);
-    removeButton->setEnabled(false);
-    removeButton->setText(tr("Remove"));
-    connect(ui.yes, &QLineEdit::textChanged, this, &RemoveFriendDialog::onTextChanged);
+    adjustSize();
     connect(ui.buttonBox, &QDialogButtonBox::accepted, this, &RemoveFriendDialog::onAccepted);
     connect(ui.buttonBox, &QDialogButtonBox::rejected, this, &RemoveFriendDialog::close);
     setFocus();
@@ -23,9 +20,3 @@ void RemoveFriendDialog::onAccepted()
     _accepted = true;
     close();
 }
-
-void RemoveFriendDialog::onTextChanged(QString text)
-{
-    ui.buttonBox->button(QDialogButtonBox::Ok)->setEnabled(text == ui.yes->placeholderText());
-}
-
