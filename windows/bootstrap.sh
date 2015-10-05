50255e8926d15f516b971a308ba2a94c9af6f5c8
Zetok Zalbavar <zetok@openmailbox.org>
2015-10-05 12:40:06 +0100

    [windows/bootstrap.sh] remove deprecated opencv part

--- windows/bootstrap.sh
+++ windows/bootstrap.sh
@@ -89,58 +89,6 @@ if [ ! -f "bin/OpenAL32.dll" ]; then
     popd
 fi
 
-## opencv
-if [ ! -f "opencv-2.4.9.tar.gz" ]; then
-    wget --no-check-certificate https://github.com/Itseez/opencv/archive/2.4.9.tar.gz -O opencv-2.4.9.tar.gz
-    rm -rf opencv-2.4.9
-fi
-
-if [ ! -d "opencv-2.4.9" ]; then
-    tar -xvf opencv-2.4.9.tar.gz
-    rm bin/libopencv_core249.dll
-fi
-
-if [ ! -f "bin/libopencv_core249.dll" ]; then
-    mkdir opencv-2.4.9/build
-    pushd opencv-2.4.9/build
-    cmake -G "MSYS Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$QTOX_DIR/libs \
-        -DBUILD_opencv_apps=NO \
-        -DBUILD_opencv_calib3d=NO \
-        -DBUILD_opencv_contrib=NO \
-        -DBUILD_opencv_core=YES \
-        -DBUILD_opencv_features2d=NO \
-        -DBUILD_opencv_flann=NO \
-        -DBUILD_opencv_gpu=NO \
-        -DBUILD_opencv_highgui=YES \
-        -DBUILD_opencv_imgproc=YES \
-        -DBUILD_opencv_legacy=NO \
-        -DBUILD_opencv_ml=NO \
-        -DBUILD_opencv_nonfree=NO \
-        -DBUILD_opencv_objdetect=NO \
-        -DBUILD_opencv_ocl=NO \
-        -DBUILD_opencv_photo=NO \
-        -DBUILD_opencv_stiching=NO \
-        -DBUILD_opencv_superres=NO \
-        -DBUILD_opencv_ts=NO \
-        -DBUILD_opencv_video=NO \
-        -DBUILD_opencv_videostab=NO \
-        -DBUILD_opencv_world=NO \
-        -DWITH_QT=NO \
-        -DBUILD_EXAMPLES=NO \
-        ..
-
-    make
-    make install
-    for arch in x86 x64
-    do
-        if [ -d $QTOX_DIR/libs/$arch/mingw ]; then
-            mv $QTOX_DIR/libs/$arch/mingw/bin/* $QTOX_DIR/libs/bin/
-            mv $QTOX_DIR/libs/$arch/mingw/lib/* $QTOX_DIR/libs/lib/
-            rm -rf $QTOX_DIR/libs/$arch
-        fi
-    done
-    popd
-fi
 
 ## ffmpeg
 if [ ! -f "ffmpeg-2.7.tar.bz2" ]; then
