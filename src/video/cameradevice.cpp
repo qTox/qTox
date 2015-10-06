qTox/qTox

commit 8776682d44803abfb217194396ad4fd2b9c33052

cameradevice: fixed a major leak: avformat_context not always freed

src/video/cameradevice.cpp
@@ -225,7 +225,6 @@ QVector<QPair<QString, QString>> CameraDevice::getRawDeviceListGeneric()
     if (av_opt_set_dict2(s, &tmp, AV_OPT_SEARCH_CHILDREN) < 0)
     {
         av_dict_free(&tmp);
-        avformat_free_context(s);
     }
     avdevice_list_devices(s, &devlist);
     if (!devlist)
@@ -243,6 +242,7 @@ QVector<QPair<QString, QString>> CameraDevice::getRawDeviceListGeneric()
         devices[i].second = dev->device_description;
     }
     avdevice_free_list_devices(&devlist);
+    avformat_free_context(s);
     return devices;
 }
 

commit c491f79077935183c9cdbe67c28bff01ec47a974

cameradevice: fixed a regression
Memory would sometimes not be freed.
Caused by 8776682d44803abfb217194396ad4fd2b9c33052

src/video/cameradevice.cpp
@@ -225,8 +225,11 @@ QVector<QPair<QString, QString>> CameraDevice::getRawDeviceListGeneric()
     if (av_opt_set_dict2(s, &tmp, AV_OPT_SEARCH_CHILDREN) < 0)
     {
         av_dict_free(&tmp);
+        avformat_free_context(s);
     }
     avdevice_list_devices(s, &devlist);
+    av_dict_free(&tmp);
+    avformat_free_context(s);
     if (!devlist)
     {
         qWarning() << "avdevice_list_devices failed";
@@ -242,7 +245,6 @@ QVector<QPair<QString, QString>> CameraDevice::getRawDeviceListGeneric()
         devices[i].second = dev->device_description;
     }
     avdevice_free_list_devices(&devlist);
-    avformat_free_context(s);
     return devices;
 }
 