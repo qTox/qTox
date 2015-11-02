3675428c6535aded3137b131e5eed5bebf665417
agilob <info@agilob.net>
2015-11-02 19:44:17 +0000

    Simplify hardcoding qt version in settings

--- src/widget/form/settings/aboutform.cpp
+++ src/widget/form/settings/aboutform.cpp
@@ -46,8 +46,7 @@ void AboutForm::replaceVersions()
                                                        QString::number(TOX_VERSION_MAJOR) + "." +
                                                        QString::number(TOX_VERSION_MINOR) + "." +
                                                        QString::number(TOX_VERSION_PATCH)));
-    bodyUI->qtVersion->setText(
-                bodyUI->qtVersion->text().replace("$QTVERSION", QT_VERSION_STR));
+    bodyUI->qtVersion->setText(tr("Qt version:") + " " + QT_VERSION_STR);
 }
 
 AboutForm::~AboutForm()
