a7d8600deb3773cebe0c6c4f3a14e1ad43cd5116
sudden6 <sudden6@users.noreply.github.com>
2016-02-14 12:17:11 +0100

    comments in doxygen style

--- src/audio/audiofilterer.h
+++ src/audio/audiofilterer.h
@@ -36,16 +36,22 @@ public:
     void startFilter(uint32_t fs);
     void closeFilter();
 
-    /* Enable/disable filters. 1 to enable, 0 to disable.
-     * echo : enables/disables echo cancelation
-     * noise: enables/disables noise reduction
-     * gain : enables/disables automatic gain control
-     * vad  : enables/disables voice activity detection
-     * echo cancelation needs a precise value set by setEchoDelayMs, else it adds echo! */
+    /**
+    Enable/disable filters. 1 to enable, 0 to disable.
+
+    @param[in] echo    enables/disables echo cancelation
+    @param[in] noise   enables/disables noise reduction
+    @param[in] gain    enables/disables automatic gain control
+    @param[in] vad     enables/disables voice activity detection
+
+    Echo cancelation needs a precise value for the input latency set by setEchoDelayMs(), else it adds echo!
+    */
     bool enableDisableFilters(int echo, int noise, int gain, int vad);
 
-    /* does the actual filtering
-     * returns True if voice was detected */
+    /**
+     * Does the actual filtering
+     * @return True if voice was detected
+    */
     bool filterAudio(int16_t* data, unsigned int samples);
 
     /* Give the audio output from your software to this function so it knows what echo to cancel from the frame */
