27faec918a023c5bba4b6ee67854438507220b35
Polshakov Dmitry <polsha3@gmail.com>
2016-04-19 12:44:46 +0300

    fix(genericchatitemwidget, micfeedbackwidget): Added members init in the constructor

--- src/widget/tool/micfeedbackwidget.cpp
+++ src/widget/tool/micfeedbackwidget.cpp
@@ -23,7 +23,7 @@
 #include <QLinearGradient>
 
 MicFeedbackWidget::MicFeedbackWidget(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent), mMeterListener(nullptr)
 {
     setFixedHeight(20);
 }
