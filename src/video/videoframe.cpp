38b1a9b63dabdd9b71e7824c602f9ff77e99eb1e
initramfs <cputerminator@hotmail.com>
2016-04-20 23:58:31 -0400

    feat(video): redesign and improve VideoFrame class
    This commit redesigns the VideoFrame class to be more robust,
    documented and performant

--- src/video/videoframe.cpp
+++ src/video/videoframe.cpp
@@ -17,294 +17,315 @@
     along with qTox.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <iostream>
+#include "videoframe.h"
 
-#include <QMutexLocker>
-#include <QDebug>
-#include <vpx/vpx_image.h>
-extern "C" {
-#include <libavcodec/avcodec.h>
+extern "C"{
 #include <libavutil/imgutils.h>
 #include <libswscale/swscale.h>
 }
-#include "videoframe.h"
-#include "camerasource.h"
 
-/**
-@class VideoFrame
-
-VideoFrame takes ownership of an AVFrame* and allows fast conversions to other formats
-Ownership of all video frame buffers is kept by the VideoFrame, even after conversion
-All references to the frame data become invalid when the VideoFrame is deleted
-We try to avoid pixel format conversions as much as possible, at the cost of some memory
-All methods are thread-safe. If provided freelistCallback will be called by the destructor,
-unless releaseFrame was called in between.
-*/
-
-VideoFrame::VideoFrame(AVFrame* frame, int w, int h, int fmt, std::function<void()> freelistCallback)
-    : freelistCallback{freelistCallback},
-      frameOther{nullptr}, frameYUV420{nullptr}, frameRGB24{nullptr},
-      width{w}, height{h}, pixFmt{fmt}
+VideoFrame::VideoFrame(AVFrame* sourceFrame, QRect dimensions, int pixFmt, std::function<void ()> destructCallback)
+    : sourceDimensions(dimensions),
+      sourcePixelFormat(pixFmt),
+      destructCallback(destructCallback)
 {
-    // Silences pointless swscale warning spam
-    // See libswscale/utils.c:1153 @ 74f0bd3
-    frame->color_range = AVCOL_RANGE_MPEG;
-    if (pixFmt == AV_PIX_FMT_YUVJ420P)
-        pixFmt = AV_PIX_FMT_YUV420P;
-    else if (pixFmt == AV_PIX_FMT_YUVJ411P)
-        pixFmt = AV_PIX_FMT_YUV411P;
-    else if (pixFmt == AV_PIX_FMT_YUVJ422P)
-        pixFmt = AV_PIX_FMT_YUV422P;
-    else if (pixFmt == AV_PIX_FMT_YUVJ444P)
-        pixFmt = AV_PIX_FMT_YUV444P;
-    else if (pixFmt == AV_PIX_FMT_YUVJ440P)
-        pixFmt = AV_PIX_FMT_YUV440P;
-    else
-        frame->color_range = AVCOL_RANGE_UNSPECIFIED;
-
-    if (pixFmt == AV_PIX_FMT_YUV420P) {
-        frameYUV420 = frame;
-    } else if (pixFmt == AV_PIX_FMT_RGB24) {
-        frameRGB24 = frame;
-    } else {
-        frameOther = frame;
-    }
+    frameBuffer[pixFmt][dimensionsToKey(dimensions.size())] = sourceFrame;
 }
 
-VideoFrame::VideoFrame(AVFrame* frame, std::function<void()> freelistCallback)
-    : VideoFrame{frame, frame->width, frame->height, frame->format, freelistCallback}
-{
-}
+VideoFrame::VideoFrame(AVFrame* sourceFrame, std::function<void ()> destructCallback)
+    : VideoFrame(sourceFrame, QRect {0, 0, sourceFrame->width, sourceFrame->height}, sourceFrame->format, destructCallback){}
 
-VideoFrame::VideoFrame(AVFrame* frame)
-    : VideoFrame{frame, frame->width, frame->height, frame->format, nullptr}
-{
-}
+VideoFrame::VideoFrame(AVFrame* sourceFrame)
+    : VideoFrame(sourceFrame, QRect {0, 0, sourceFrame->width, sourceFrame->height}, sourceFrame->format, nullptr){}
 
-/**
-@brief VideoFrame constructor. Disable copy.
-@note Use a shared_ptr if you need copies.
-*/
 VideoFrame::~VideoFrame()
 {
-    if (freelistCallback)
-        freelistCallback();
+    frameLock.lockForWrite();
 
-    releaseFrameLockless();
+    if(destructCallback && frameBuffer.size() > 0)
+    {
+        destructCallback();
+    }
+
+    deleteFrameBuffer();
+
+    frameLock.unlock();
 }
 
-/**
-@brief Converts the VideoFrame to a QImage that shares our internal video buffer.
-@param size Size of resulting image.
-@return Converted image to RGB24 color model.
-*/
-QImage VideoFrame::toQImage(QSize size)
+bool VideoFrame::isValid()
 {
-    if (!convertToRGB24(size))
-        return QImage();
+    frameLock.lockForRead();
+    bool retValue = frameBuffer.size() > 0;
+    frameLock.unlock();
 
-    QMutexLocker locker(&biglock);
-
-    return QImage(*frameRGB24->data, frameRGB24->width, frameRGB24->height, *frameRGB24->linesize, QImage::Format_RGB888);
+    return retValue;
 }
 
-/**
-@brief Converts the VideoFrame to a vpx_image_t.
-Converts the VideoFrame to a vpx_image_t that shares our internal video buffer.
-@return Converted image to vpx_image format.
-*/
-vpx_image *VideoFrame::toVpxImage()
+const AVFrame* VideoFrame::getAVFrame(const QSize& dimensions, const int pixelFormat)
 {
-    vpx_image* img = vpx_img_alloc(nullptr, VPX_IMG_FMT_I420, width, height, 0);
+    frameLock.lockForRead();
 
-    if (!convertToYUV420())
-        return img;
-
-    for (int i = 0; i < 3; i++)
+    // We return nullptr if the VideoFrame is no longer valid
+    if(frameBuffer.size() == 0)
     {
-        int dstStride = img->stride[i];
-        int srcStride = frameYUV420->linesize[i];
-        int minStride = std::min(dstStride, srcStride);
-        int size = (i == 0) ? img->d_h : img->d_h / 2;
-
-        for (int j = 0; j < size; j++)
-        {
-            uint8_t *dst = img->planes[i] + dstStride * j;
-            uint8_t *src = frameYUV420->data[i] + srcStride * j;
-            memcpy(dst, src, minStride);
-        }
+        frameLock.unlock();
+        return nullptr;
     }
-    return img;
+
+    AVFrame* ret = retrieveAVFrame(dimensions, pixelFormat);
+
+    if(ret)
+    {
+        frameLock.unlock();
+        return ret;
+    }
+
+    // VideoFrame does not contain an AVFrame to spec, generate one here
+    ret = generateAVFrame(dimensions, pixelFormat);
+
+    /*
+     * We need to "upgrade" the lock to a write lock so we can update our frameBuffer map.
+     *
+     * It doesn't matter if another thread obtains the write lock before we finish since it is
+     * likely writing to somewhere else. Absolute worst-case scenario, we merely perform the
+     * generation process twice, and discard the old result.
+     */
+    frameLock.unlock();
+    frameLock.lockForWrite();
+
+    storeAVFrame(ret, dimensions, pixelFormat);
+
+    frameLock.unlock();
+    return ret;
 }
 
-bool VideoFrame::convertToRGB24(QSize size)
-{
-    QMutexLocker locker(&biglock);
-
-    AVFrame* sourceFrame;
-    if (frameOther)
-    {
-        sourceFrame = frameOther;
-    }
-    else if (frameYUV420)
-    {
-        sourceFrame = frameYUV420;
-    }
-    else
-    {
-        qWarning() << "None of the frames are valid! Did someone release us?";
-        return false;
-    }
-    //std::cout << "converting to RGB24" << std::endl;
-
-    if (size.isEmpty())
-    {
-        size.setWidth(sourceFrame->width);
-        size.setHeight(sourceFrame->height);
-    }
-
-    if (frameRGB24)
-    {
-        if (frameRGB24->width == size.width() && frameRGB24->height == size.height())
-            return true;
-
-        av_free(frameRGB24->opaque);
-        av_frame_unref(frameRGB24);
-        av_frame_free(&frameRGB24);
-    }
-
-    frameRGB24=av_frame_alloc();
-    if (!frameRGB24)
-    {
-        qCritical() << "av_frame_alloc failed";
-        return false;
-    }
-
-    int imgBufferSize = av_image_get_buffer_size(AV_PIX_FMT_RGB24, size.width(), size.height(), 1);
-    uint8_t* buf = (uint8_t*)av_malloc(imgBufferSize);
-    if (!buf)
-    {
-        qCritical() << "av_malloc failed";
-        av_frame_free(&frameRGB24);
-        return false;
-    }
-    frameRGB24->opaque = buf;
-
-    uint8_t** data = frameRGB24->data;
-    int* linesize = frameRGB24->linesize;
-    av_image_fill_arrays(data, linesize, buf, AV_PIX_FMT_RGB24, size.width(), size.height(), 1);
-    frameRGB24->width = size.width();
-    frameRGB24->height = size.height();
-
-    // Bilinear is better for shrinking, bicubic better for upscaling
-    int resizeAlgo = size.width()<=width ? SWS_BILINEAR : SWS_BICUBIC;
-
-    SwsContext *swsCtx =  sws_getContext(width, height, (AVPixelFormat)pixFmt,
-                                          size.width(), size.height(), AV_PIX_FMT_RGB24,
-                                          resizeAlgo, nullptr, nullptr, nullptr);
-    sws_scale(swsCtx, (uint8_t const * const *)sourceFrame->data,
-                sourceFrame->linesize, 0, height,
-                frameRGB24->data, frameRGB24->linesize);
-    sws_freeContext(swsCtx);
-
-    return true;
-}
-
-bool VideoFrame::convertToYUV420()
-{
-    QMutexLocker locker(&biglock);
-
-    if (frameYUV420)
-        return true;
-
-    AVFrame* sourceFrame;
-    if (frameOther)
-    {
-        sourceFrame = frameOther;
-    }
-    else if (frameRGB24)
-    {
-        sourceFrame = frameRGB24;
-    }
-    else
-    {
-        qCritical() << "None of the frames are valid! Did someone release us?";
-        return false;
-    }
-    //std::cout << "converting to YUV420" << std::endl;
-
-    frameYUV420=av_frame_alloc();
-    if (!frameYUV420)
-    {
-        qCritical() << "av_frame_alloc failed";
-        return false;
-    }
-
-    int imgBufferSize = av_image_get_buffer_size(AV_PIX_FMT_RGB24, width, height, 1);
-    uint8_t* buf = (uint8_t*)av_malloc(imgBufferSize);
-    if (!buf)
-    {
-        qCritical() << "av_malloc failed";
-        av_frame_free(&frameYUV420);
-        return false;
-    }
-    frameYUV420->opaque = buf;
-
-    uint8_t** data = frameYUV420->data;
-    int* linesize = frameYUV420->linesize;
-    av_image_fill_arrays(data, linesize, buf, AV_PIX_FMT_YUV420P, width, height, 1);
-
-    SwsContext *swsCtx =  sws_getContext(width, height, (AVPixelFormat)pixFmt,
-                                          width, height, AV_PIX_FMT_YUV420P,
-                                          SWS_BILINEAR, nullptr, nullptr, nullptr);
-    sws_scale(swsCtx, (uint8_t const * const *)sourceFrame->data,
-                sourceFrame->linesize, 0, height,
-                frameYUV420->data, frameYUV420->linesize);
-    sws_freeContext(swsCtx);
-
-    return true;
-}
-
-/**
-@brief Frees all frame memory.
-
-Frees all internal buffers and frame data, removes the freelistCallback
-This makes all converted objects that shares our internal buffers invalid.
-*/
 void VideoFrame::releaseFrame()
 {
-    QMutexLocker locker(&biglock);
-    freelistCallback = nullptr;
-    releaseFrameLockless();
+    frameLock.lockForWrite();
+
+    deleteFrameBuffer();
+
+    frameLock.unlock();
 }
 
-void VideoFrame::releaseFrameLockless()
+QImage VideoFrame::toQImage(QSize frameSize)
 {
-    if (frameOther)
+    frameLock.lockForRead();
+
+    // We return an empty QImage if the VideoFrame is no longer valid
+    if(frameBuffer.size() == 0)
     {
-        av_free(frameOther->opaque);
-        av_frame_unref(frameOther);
-        av_frame_free(&frameOther);
+        frameLock.unlock();
+        return QImage {};
     }
-    if (frameYUV420)
+
+    if(frameSize.width() == 0 && frameSize.height() == 0)
     {
-        av_free(frameYUV420->opaque);
-        av_frame_unref(frameYUV420);
-        av_frame_free(&frameYUV420);
+        frameSize = sourceDimensions.size();
     }
-    if (frameRGB24)
+
+    AVFrame* frame = retrieveAVFrame(frameSize, static_cast<int>(AV_PIX_FMT_RGB24));
+
+    if(frame)
     {
-        av_free(frameRGB24->opaque);
-        av_frame_unref(frameRGB24);
-        av_frame_free(&frameRGB24);
+        QImage ret {*(frame->data), frameSize.width(), frameSize.height(), *(frame->linesize), QImage::Format_RGB888};
+
+        frameLock.unlock();
+        return ret;
+    }
+
+    // VideoFrame does not contain an AVFrame to spec, generate one here
+    frame = generateAVFrame(frameSize, static_cast<int>(AV_PIX_FMT_RGB24));
+
+    /*
+     * We need to "upgrade" the lock to a write lock so we can update our frameBuffer map.
+     *
+     * It doesn't matter if another thread obtains the write lock before we finish since it is
+     * likely writing to somewhere else. Absolute worst-case scenario, we merely perform the
+     * generation process twice, and discard the old result.
+     */
+    frameLock.unlock();
+    frameLock.lockForWrite();
+
+    storeAVFrame(frame, frameSize, static_cast<int>(AV_PIX_FMT_RGB24));
+
+    QImage ret {*(frame->data), frameSize.width(), frameSize.height(), *(frame->linesize), QImage::Format_RGB888};
+
+    frameLock.unlock();
+    return ret;
+}
+
+vpx_image* VideoFrame::toVpxImage(QSize frameSize)
+{
+    frameLock.lockForRead();
+
+    // We return nullptr if the VideoFrame is no longer valid
+    if(frameBuffer.size() == 0)
+    {
+        frameLock.unlock();
+        return nullptr;
+    }
+
+    if(frameSize.width() == 0 && frameSize.height() == 0)
+    {
+        frameSize = sourceDimensions.size();
+    }
+
+    AVFrame* frame = retrieveAVFrame(frameSize, static_cast<int>(AV_PIX_FMT_YUV420P));
+
+    if(frame)
+    {
+        vpx_image* ret = new vpx_image {};
+        memset(ret, 0, sizeof(vpx_image));
+
+        ret->w = ret->d_w = frameSize.width();
+        ret->h = ret->d_h = frameSize.height();
+        ret->fmt = VPX_IMG_FMT_I420;
+        ret->planes[0] = frame->data[0];
+        ret->planes[1] = frame->data[1];
+        ret->planes[2] = frame->data[2];
+        ret->planes[3] = nullptr;
+        ret->stride[0] = frame->linesize[0];
+        ret->stride[1] = frame->linesize[1];
+        ret->stride[2] = frame->linesize[2];
+        ret->stride[3] = frame->linesize[3];
+
+        frameLock.unlock();
+        return ret;
+    }
+
+    // VideoFrame does not contain an AVFrame to spec, generate one here
+    frame = generateAVFrame(frameSize, static_cast<int>(AV_PIX_FMT_YUV420P));
+
+    /*
+     * We need to "upgrade" the lock to a write lock so we can update our frameBuffer map.
+     *
+     * It doesn't matter if another thread obtains the write lock before we finish since it is
+     * likely writing to somewhere else. Absolute worst-case scenario, we merely perform the
+     * generation process twice, and discard the old result.
+     */
+    frameLock.unlock();
+    frameLock.lockForWrite();
+
+    storeAVFrame(frame, frameSize, static_cast<int>(AV_PIX_FMT_YUV420P));
+
+    vpx_image* ret = new vpx_image {};
+    memset(ret, 0, sizeof(vpx_image));
+
+    ret->w = ret->d_w = frameSize.width();
+    ret->h = ret->d_h = frameSize.height();
+    ret->fmt = VPX_IMG_FMT_I420;
+    ret->planes[0] = frame->data[0];
+    ret->planes[1] = frame->data[1];
+    ret->planes[2] = frame->data[2];
+    ret->planes[3] = nullptr;
+    ret->stride[0] = frame->linesize[0];
+    ret->stride[1] = frame->linesize[1];
+    ret->stride[2] = frame->linesize[2];
+    ret->stride[3] = frame->linesize[3];
+
+    frameLock.unlock();
+    return ret;
+}
+
+AVFrame* VideoFrame::retrieveAVFrame(const QSize& dimensions, const int pixelFormat)
+{
+    if(frameBuffer.contains(pixelFormat) && frameBuffer[pixelFormat].contains(dimensionsToKey(dimensions)))
+    {
+        return frameBuffer[pixelFormat][dimensionsToKey(dimensions)];
+    }
+    else
+    {
+        return nullptr;
     }
 }
 
-/**
-@brief Return the size of the original frame
-@return The size of the original frame
-*/
-QSize VideoFrame::getSize()
+AVFrame* VideoFrame::generateAVFrame(const QSize& dimensions, const int pixelFormat)
 {
-    return {width, height};
+    AVFrame* ret = av_frame_alloc();
+
+    if(!ret){
+        return nullptr;
+    }
+
+    int bufSize = av_image_alloc(ret->data, ret->linesize,
+                                 dimensions.width(), dimensions.height(),
+                                 static_cast<AVPixelFormat>(pixelFormat), data_alignment);
+
+    if(bufSize < 0){
+        av_frame_free(&ret);
+        return nullptr;
+    }
+
+    // Bilinear is better for shrinking, bicubic better for upscaling
+    int resizeAlgo = sourceDimensions.width() > dimensions.width() ? SWS_BILINEAR : SWS_BICUBIC;
+
+    SwsContext* swsCtx =  sws_getContext(sourceDimensions.width(), sourceDimensions.height(),
+                                         static_cast<AVPixelFormat>(sourcePixelFormat),
+                                         dimensions.width(), dimensions.height(),
+                                         static_cast<AVPixelFormat>(pixelFormat),
+                                         resizeAlgo, nullptr, nullptr, nullptr);
+
+    if(!swsCtx){
+        av_frame_free(&ret);
+        return nullptr;
+    }
+
+    AVFrame* source = frameBuffer[sourcePixelFormat][dimensionsToKey(sourceDimensions.size())];
+
+    sws_scale(swsCtx, source->data, source->linesize, 0, sourceDimensions.height(), ret->data, ret->linesize);
+
+    // Populate AVFrame fields
+    ret->width = dimensions.width();
+    ret->height = dimensions.height();
+    ret->format = pixelFormat;
+
+    sws_freeContext(swsCtx);
+    return ret;
+}
+
+void VideoFrame::storeAVFrame(AVFrame* frame, const QSize& dimensions, const int pixelFormat)
+{
+    // We check the prescence of the frame in case of double-computation
+    if(frameBuffer.contains(pixelFormat) && frameBuffer[pixelFormat].contains(dimensionsToKey(dimensions)))
+    {
+        AVFrame* old_ret = frameBuffer[pixelFormat][dimensionsToKey(dimensions)];
+
+        // Free old frame
+        av_freep(&frame->data[0]);
+        av_frame_unref(old_ret);
+        av_frame_free(&old_ret);
+    }
+
+    frameBuffer[pixelFormat].insert(dimensionsToKey(dimensions), frame);
+}
+
+void VideoFrame::deleteFrameBuffer()
+{
+    const quint64 sourceKey = dimensionsToKey(sourceDimensions.size());
+
+    for(auto pixFmtIter = frameBuffer.begin(); pixFmtIter != frameBuffer.end(); ++pixFmtIter)
+    {
+        const auto& pixFmtMap = pixFmtIter.value();
+
+        for(auto dimKeyIter = pixFmtMap.begin(); dimKeyIter != pixFmtMap.end(); ++dimKeyIter)
+        {
+            AVFrame* frame = dimKeyIter.value();
+
+            // We only need to free allocated buffers for derived frames and not the original source
+            if(pixFmtIter.key() == sourcePixelFormat && dimKeyIter.key() == sourceKey)
+            {
+                av_frame_unref(frame);
+                av_frame_free(&frame);
+            }
+            else
+            {
+                av_freep(&frame->data[0]);
+                av_frame_unref(frame);
+                av_frame_free(&frame);
+            }
+        }
+    }
+
+    frameBuffer.clear();
 }
