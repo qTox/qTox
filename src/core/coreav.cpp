c6b156bd9dbfd587a9b13aa50bbe7d766ae9d877
initramfs <cputerminator@hotmail.com>
2016-04-23 20:51:54 -0400

    fix(video): fix CoreAV and VideoSurface to conform to new VideoFrame

--- src/core/coreav.cpp
+++ src/core/coreav.cpp
@@ -360,7 +360,12 @@ void CoreAV::sendCallVideo(uint32_t callId, shared_ptr<VideoFrame> vframe)
 
     // This frame shares vframe's buffers, we don't call vpx_img_free but just delete it
     vpx_image* frame = vframe->toVpxImage();
-    if (frame->fmt == VPX_IMG_FMT_NONE)
+
+    if(!frame)
+    {
+        return;
+    }
+    if(frame->fmt == VPX_IMG_FMT_NONE)
     {
         qWarning() << "Invalid frame";
         vpx_img_free(frame);
