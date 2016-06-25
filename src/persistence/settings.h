6ddd8c1df85e62ee4859fa4a0e7988f77b47c4c6
Jeanbon <jean@arbomou.ovh>
2016-06-25 21:03:52 +0200

    feat(autoAnswer): add a new param for autoAcceptCall

--- src/persistence/settings.h
+++ src/persistence/settings.h
@@ -397,6 +397,9 @@ public:
     QString getAutoAcceptDir(const ToxId& id) const;
     void setAutoAcceptDir(const ToxId& id, const QString& dir);
 
+    bool getAutoAcceptCall(const ToxId& id) const;
+    void setAutoAcceptCall(const ToxId& id, bool accept);
+
     QString getGlobalAutoAcceptDir() const;
     void setGlobalAutoAcceptDir(const QString& dir);
 
@@ -620,6 +623,7 @@ private:
         QString note;
         int circleID = -1;
         QDate activity = QDate();
+        bool autoAcceptCall;
     };
 
     struct circleProp
