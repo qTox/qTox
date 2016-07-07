d601599de8c2d18b98f5fabd1b8ac78468fada8e
a68366 <a68366@yandex.com>
2016-07-08 00:18:12 +0300

    fix(generalform): call UI retranslation when date or time format changes
    Needed to update date and time display.

--- src/widget/form/settings/generalform.cpp
+++ src/widget/form/settings/generalform.cpp
@@ -355,11 +355,13 @@ void GeneralForm::onEmoticonSizeChanged()
 void GeneralForm::onTimestampSelected(int index)
 {
     Settings::getInstance().setTimestampFormat(timeFormats.at(index));
+    Translator::translate();
 }
 
 void GeneralForm::onDateFormatSelected(int index)
 {
     Settings::getInstance().setDateFormat(dateFormats.at(index));
+    Translator::translate();
 }
 
 void GeneralForm::onAutoAwayChanged()
