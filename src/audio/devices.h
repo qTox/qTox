704799d21f4dd4980553a0b0c3658bb26e8df987
Nils Fenner <nilsfenner@web.de>
2016-07-29 00:20:12 +0200

    feat(audio): changed Audio namespace into a non-constructable class
    Makes API usage a bit easier solves some "unused static function" warnings.

--- src/audio/devices.h
+++ src/audio/devices.h
@@ -26,127 +26,145 @@
 #include <QSharedData>
 
 namespace qTox {
-namespace Audio {
 
-enum class Format
+class Audio
 {
-    SINT8   = 0x01,
-    SINT16  = 0x02,
-    SINT24  = 0x04,
-    SINT32  = 0x08,
-    FLOAT32 = 0x10,
-    FLOAT64 = 0x20
-};
-
-static inline quint8 formatSize(Format fmt)
-{
-    switch (fmt)
+public:
+    enum class Format
     {
-    case Format::SINT8: return 1;
-    case Format::SINT16: return 2;
-    case Format::SINT24: return 3;
-    case Format::SINT32:
-    case Format::FLOAT32: return 4;
-    case Format::FLOAT64: return 8;
-    }
-}
+        SINT8   = 0x01,
+        SINT16  = 0x02,
+        SINT24  = 0x04,
+        SINT32  = 0x08,
+        FLOAT32 = 0x10,
+        FLOAT64 = 0x20
+    };
 
-typedef std::function<int (const void* pcm, Format fmt, size_t size,
-                           quint8 channels, quint32 sampleRate)> RecordFunc;
-
-class Device
-{
-    class Private;
-
-    friend class StreamContext;
-
-public:
-    typedef QVector<Device> List;
-
-public:
-    static List availableDevices();
-    static int find(const QString& name);
-
-public:
-    Device(Private* p = nullptr);
-    Device(const Device& other);
-    Device(Device&& other);
-    ~Device();
-
-    Device& operator=(const Device& other);
-    Device& operator=(Device&& other);
-
-public:
-    inline bool isNull() const
+    static inline quint8 formatSize(Format fmt)
     {
-        return !d;
+        switch (fmt)
+        {
+        case Format::SINT8: return 1;
+        case Format::SINT16: return 2;
+        case Format::SINT24: return 3;
+        case Format::SINT32:
+        case Format::FLOAT32: return 4;
+        case Format::FLOAT64: return 8;
+        }
+
+        return 0;
     }
 
-    bool isValid() const;
-    QString name() const;
-    quint32 inputChannels() const;
-    quint32 outputChannels() const;
-    quint32 duplexChannels() const;
-    bool isOutput() const;
-    bool isDefaultOutput() const;
-    bool isInput() const;
-    bool isDefaultInput() const;
+public:
+    typedef std::function<int (const void* pcm, Format fmt, size_t frames,
+                               quint8 channels, quint32 sampleRate)> RecordFunc;
+
+public:
+    class Device
+    {
+    public:
+        class Private;
+
+    public:
+        typedef QVector<Device> List;
+
+    public:
+        static int find(const QString& name);
+
+    public:
+        Device(Private* p = nullptr);
+        Device(const Device& other);
+        Device(Device&& other);
+        ~Device();
+
+        Device& operator=(const Device& other);
+        Device& operator=(Device&& other);
+
+    public:
+        inline bool isNull() const
+        {
+            return !d;
+        }
+
+        bool isValid() const;
+        QString name() const;
+        quint32 inputChannels() const;
+        quint32 outputChannels() const;
+        quint32 duplexChannels() const;
+        bool isOutput() const;
+        bool isDefaultOutput() const;
+        bool isInput() const;
+        bool isDefaultInput() const;
+
+    private:
+        QExplicitlySharedDataPointer<Private> d;
+    };
+
+    class StreamContext
+    {
+    public:
+        class Private;
+
+    public:
+        StreamContext();
+        StreamContext(const StreamContext& other);
+        StreamContext(StreamContext&& other);
+        ~StreamContext();
+
+        StreamContext& operator=(const StreamContext& other);
+        StreamContext& operator=(StreamContext&& other);
+
+        inline bool isNull() const
+        {
+            return !d;
+        }
+
+
+
+        int inputDeviceId() const;
+        Device inputDevie() const;
+        void setInputDevice(int deviceId = -1);
+        void removeInputDevice();
+
+        int outputDeviceId() const;
+        Device outputDevice() const;
+        void setOutputDevice(int deviceId = -1);
+        void removeOutputDevice();
+
+        bool open();
+        void close();
+
+        bool abort();
+        bool start();
+        bool stop();
+
+        bool isOpen() const;
+        bool isRunning() const;
+
+        void playback(char* pcm, Format fmt, quint32 frames, quint8 channels,
+                      quint32 sampleRate);
+        void playback(const QString& fileName);
+
+    public:
+        void onRecorded(RecordFunc event);
+
+    private:
+        QExplicitlySharedDataPointer<Private> d;
+    };
+
+public:
+    static Device::List availableDevices();
 
 private:
-    QExplicitlySharedDataPointer<Private> d;
+    Audio() = delete;
+    Audio(const Audio&) = delete;
+    Audio(Audio&&) = delete;
+    ~Audio() = delete;
+
+    Audio& operator=(const Audio&) = delete;
+    Audio& operator=(Audio&&) = delete;
 };
 
-class StreamContext
-{
-public:
-    class Private;
-
-public:
-    StreamContext();
-    StreamContext(const StreamContext& other);
-    StreamContext(StreamContext&& other);
-    ~StreamContext();
-
-    StreamContext& operator=(const StreamContext& other);
-    StreamContext& operator=(StreamContext&& other);
-
-    inline bool isNull() const
-    {
-        return !d;
-    }
-
-    int inputDeviceId() const;
-    Device inputDevie() const;
-    void setInputDevice(int deviceId = -1);
-    void removeInputDevice();
-
-    int outputDeviceId() const;
-    Device outputDevice() const;
-    void setOutputDevice(int deviceId = -1);
-    void removeOutputDevice();
-
-    bool open();
-    void close();
-
-    bool abort();
-    bool start();
-    bool stop();
-
-    bool isOpen() const;
-    bool isRunning() const;
-
-    void playback(char* pcm, Format fmt, quint32 frames, quint8 channels,
-                  quint32 sampleRate);
-    void playback(const QString& fileName);
-
-public:
-    void onRecorded(RecordFunc event);
-
-private:
-    QExplicitlySharedDataPointer<Private> d;
-};
-
-}
 }
 
 Q_DECLARE_METATYPE(qTox::Audio::Device)
