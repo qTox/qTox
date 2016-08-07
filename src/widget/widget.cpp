920899369fa74c41701555b2924a322d0690e806
Nils Fenner <nilsfenner@web.de>
2016-08-07 06:36:53 +0200

    refactor: remove unused signal

--- src/widget/widget.cpp
+++ src/widget/widget.cpp
@@ -584,10 +584,8 @@ void Widget::changeEvent(QEvent* event)
 
 void Widget::resizeEvent(QResizeEvent* event)
 {
-    Q_UNUSED(event);
     saveWindowGeometry();
-
-    emit resized();
+    QMainWindow::resizeEvent(event);
 }
 
 QString Widget::getUsername()
