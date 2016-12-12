68474e7403afe314e415e6edb9cfa066013fc304
Diadlo <polsha3@gmail.com>
2016-12-12 23:00:51 +0300

    refactor: Small Generic chat form refactoring

--- src/widget/form/genericchatform.cpp
+++ src/widget/form/genericchatform.cpp
@@ -49,6 +49,16 @@
 #include "widget/translator.h"
 #include "widget/widget.h"
 
+/**
+ * @class GenericChatForm
+ * @brief Parent class for all chatforms. It's provide the minimum required UI
+ * elements and methods to work with chat messages.
+ *
+ * @var GenericChatForm::historyBaselineDate
+ * @brief Used by HistoryKeeper to load messages from t to historyBaselineDate
+ *        (excluded)
+ */
+
 GenericChatForm::GenericChatForm(QWidget *parent)
   : QWidget(parent, Qt::Window)
   , audioInputFlag(false)
