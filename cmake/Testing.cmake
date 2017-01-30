dcf5b35c05b40412913d848ee2766036a03a3cbe
Diadlo <polsha3@gmail.com>
2017-01-30 23:39:52 +0300

    chore(cmake): Add unit_test build

---
+++ cmake/Testing.cmake
@@ -0,0 +1,32 @@
+################################################################################
+#
+# :: Testing
+#
+################################################################################
+
+set(TEST_NAME unit_test)
+
+set(TEST_SOURCES
+  test/main.cpp
+)
+
+if (NOT ENABLE_UNIT_TESTS)
+  set(ENABLE_UNIT_TESTS True)
+endif()
+
+if (${ENABLE_UNIT_TESTS})
+    search_dependency(GTEST PACKAGE gtest OPTIONAL)
+    search_dependency(GMOCK PACKAGE gmock OPTIONAL)
+
+    if (GTEST_FOUND AND GMOCK_FOUND)
+        add_executable(${TEST_NAME}
+          ${TEST_SOURCES}
+        )
+
+        target_link_libraries(${TEST_NAME}
+          ${PROJECT_NAME}_static
+          ${ALL_LIBRARIES}
+          gtest gmock
+        )
+    endif()
+endif()
