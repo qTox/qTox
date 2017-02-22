e68aef19f9373544fdf7299cf8b901b4c4935aaa
noavarice <alex9razinkov@gmail.com>
2017-02-22 16:35:34 +0300

    docs: added documentation for GroupInviteForm class

--- src/groupinvite.h
+++ src/groupinvite.h
@@ -1,3 +1,22 @@
+/*
+    Copyright © 2017 by The qTox Project Contributors
+
+    This file is part of qTox, a Qt-based graphical interface for Tox.
+
+    qTox is libre software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    qTox is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with qTox.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef GROUPINVITE_H
 #define GROUPINVITE_H
 
@@ -8,7 +27,7 @@
 class GroupInvite
 {
 public:
-    GroupInvite(const int32_t FriendId, const uint8_t Type, const QByteArray& Invite);
+    GroupInvite(int32_t friendID, uint8_t inviteType, const QByteArray& data);
     bool operator ==(const GroupInvite& other) const;
 
     int32_t getFriendId() const;
