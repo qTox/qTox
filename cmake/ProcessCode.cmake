ad2a0e9fb488bea58dcdb9c8c791748876e96d4f
Zetok Zalbavar <zetok@openmailbox.org>
2017-02-26 12:13:50 +0000

    chore(build): integrate clang-format with build system and travis

---
+++ cmake/ProcessCode.cmake
@@ -0,0 +1,19 @@
+################################################################################
+#
+# :: Process Code
+#
+################################################################################
+
+# set correct version to update files
+set(QTOX_VERSION "1.8.1")
+execute_process(COMMAND ${CMAKE_SOURCE_DIR}/tools/update-versions.sh
+    ${QTOX_VERSION})
+
+# format the code when clang-format is available, but don't fail if
+# it's not available
+find_program(CLANG_FORMAT_FOUND ccache)
+if(NOT CLANG_FORMAT_FOUND)
+  message(STATUS "clang-format not found!")
+else()
+  execute_process(COMMAND ${CMAKE_SOURCE_DIR}/tools/format-code.sh)
+endif()
