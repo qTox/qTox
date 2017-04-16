571561ca1f468ae84903219aafa64469f03f6796
sudden6 <sudden6@gmx.at>
2017-04-16 16:14:04 +0200

    feat(audio): add alternative OpenAL backend

---
+++ src/audio/backend/openal2.h
@@ -0,0 +1,132 @@
+/*
+    Copyright © 2014-2017 by The qTox Project Contributors
+
+    This file is part of qTox, a Qt-based graphical interface for Tox.
+
+    qTox is libre software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    qTox is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with qTox.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef OPENAL2_H
+#define OPENAL2_H
+
+#include "src/audio/audio.h"
+
+#include <atomic>
+#include <cmath>
+
+#include <QMutex>
+#include <QObject>
+#include <QTimer>
+
+#include <cassert>
+
+#if defined(__APPLE__) && defined(__MACH__)
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+#include <OpenAL/alext.h>
+#else
+#include <AL/al.h>
+#include <AL/alc.h>
+#include <AL/alext.h>
+#endif
+
+class OpenAL2 : public Audio
+{
+    Q_OBJECT
+
+public:
+    OpenAL2();
+    ~OpenAL2();
+
+    qreal outputVolume() const;
+    void setOutputVolume(qreal volume);
+
+    qreal minInputGain() const;
+    void setMinInputGain(qreal dB);
+
+    qreal maxInputGain() const;
+    void setMaxInputGain(qreal dB);
+
+    qreal inputGain() const;
+    void setInputGain(qreal dB);
+
+    void reinitInput(const QString& inDevDesc);
+    bool reinitOutput(const QString& outDevDesc);
+
+    bool isOutputReady() const;
+
+    QStringList outDeviceNames();
+    QStringList inDeviceNames();
+
+    void subscribeOutput(uint& sourceId);
+    void unsubscribeOutput(uint& sourceId);
+
+    void subscribeInput();
+    void unsubscribeInput();
+
+    void startLoop();
+    void stopLoop();
+    void playMono16Sound(const QByteArray& data);
+    void playMono16Sound(const QString& path);
+
+    void playAudioBuffer(uint sourceId, const int16_t* data, int samples, unsigned channels,
+                         int sampleRate);
+
+signals:
+    void frameAvailable(const int16_t* pcm, size_t sample_count, uint8_t channels,
+                        uint32_t sampling_rate);
+
+private:
+
+    static void checkAlError() noexcept;
+    static void checkAlcError(ALCdevice* device) noexcept;
+
+    bool autoInitInput();
+    bool autoInitOutput();
+    bool initInput(const QString& deviceName);
+    bool initOutput(const QString& outDevDescr);
+    void cleanupInput();
+    void cleanupOutput();
+    void playMono16SoundCleanup();
+    void doAudio();
+    qreal inputGainFactor() const;
+
+private:
+    QThread* audioThread;
+    mutable QMutex audioLock;
+
+    ALCdevice* alInDev;
+    quint32 inSubscriptions;
+    QTimer captureTimer, playMono16Timer;
+
+    ALCdevice* alOutDev;
+    ALCcontext* alOutContext;
+    ALCdevice* alProxyDev;
+    ALCcontext* alProxyContext;
+    ALuint alMainSource;
+    ALuint alMainBuffer;
+    ALuint alProxySource;
+    ALuint alProxyBuffer;
+    bool outputInitialized;
+    bool echoCancelSupported = false;
+
+    QList<ALuint> peerSources;
+    qreal gain;
+    qreal gainFactor;
+    qreal minInGain;
+    qreal maxInGain;
+};
+
+#endif // OPENAL2_H
