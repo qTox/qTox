83b9a846a1d850d7a98fecc4e8a44865bc5ec42f
sudden6 <sudden6@gmx.at>
2017-06-23 17:36:07 +0200

    chore(build): build without filteraudio if lib not found

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -117,9 +117,6 @@ if (NOT TOXCORE_FOUND OR
 endif()
 
 search_dependency(OPENAL              PACKAGE openal)
-if (${USE_FILTERAUDIO})
-    search_dependency(FILTERAUDIO         LIBRARY filteraudio)
-endif()
 
 if (PLATFORM_EXTENSIONS AND UNIX AND NOT APPLE)
   # Automatic auto-away support. (X11 also using for capslock detection)
