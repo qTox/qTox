c441aa400bb0d5bcca607b5bdef5a055312bb0d7
sudden6 <sudden6@gmx.at>
2017-07-02 23:09:44 +0200

    chore(build): remove unneeded stuff

--- windows/toolchain-x86_64-w64-mingw32.cmake
+++ windows/toolchain-x86_64-w64-mingw32.cmake
@@ -6,9 +6,6 @@ SET(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 SET(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
 SET(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
 
-# here is the target environment located
-SET(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH} /usr/x86_64-w64-mingw32)
-
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
 # programs in the host environment
