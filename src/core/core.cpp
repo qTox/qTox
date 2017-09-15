36ec79e6714c50d3adfcafd8855c9a6150f88b0b
anthony.bilinski <me@abilinski.com>
2017-09-15 14:46:11 -0700

    fix(init): register AV connects and call after AV is ready
    Fixes #4406

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -245,6 +245,10 @@ void Core::makeAv()
         qCritical() << "Toxav core failed to start";
         emit failedToStart();
     }
+    for (const auto& callback : toCallWhenAvReady) {
+        callback(av);
+    }
+    toCallWhenAvReady.clear();
 }
 
 /**
@@ -1388,6 +1392,10 @@ bool Core::isReady() const
     return av && av->getToxAv() && tox && ready;
 }
 
+void Core::callWhenAvReady(const std::function<void(CoreAV* av)>& toCall)
+{
+    toCallWhenAvReady.emplace_back(toCall);
+}
 
 /**
  * @brief Sets the NoSpam value to prevent friend request spam
