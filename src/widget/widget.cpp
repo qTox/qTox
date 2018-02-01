0d2926e19619aee4be5593f0ed1915aea47fe288
Diadlo <polsha3@gmail.com>
2018-02-02 01:16:54 +0300

    refactor: Remove ChatForm from Group

--- src/widget/widget.cpp
+++ src/widget/widget.cpp
@@ -527,6 +527,10 @@ Widget::~Widget()
         delete form;
     }
 
+    for (auto form : groupChatForms) {
+        delete form;
+    }
+
     delete icon;
     delete profileForm;
     delete addFriendForm;
@@ -1848,7 +1852,7 @@ Group* Widget::createGroup(int groupId)
     GroupWidget* widget = new GroupWidget(groupId, groupName, compact);
     groupWidgets[groupId] = widget;
 
-    auto form = newgroup->getChatForm();
+    auto form = new GroupChatForm(newgroup);
     groupChatForms[groupId] = form;
 
     contactListWidget->addGroupWidget(widget);
