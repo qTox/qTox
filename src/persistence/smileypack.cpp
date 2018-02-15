qTox/qTox

commit 5df63f9c2e6d78f4799447b0a22cdb9fb70c3fea

fix(chatlog): parse multi-length emoji properly
Before, when multi-length emoji were next to any other character, they would not be transformed into images. With this change, emoji are replaced with images no matter where they are in the string. Parsing is now done by checking to see if two-character blocks are valid as a single UTF-32 character, or if they are truly two distinct characters. Because we no longer need white space before emoji, I also removed our addition of spaces on either side of an emoji when a user sends them.

src/persistence/smileypack.cpp
@@ -258,21 +258,25 @@ QString SmileyPack::smileyfied(const QString& msg)
 {
     QMutexLocker locker(&loadingMutex);
     QString result(msg);
-    QRegularExpression exp("\\S+");
-    QRegularExpressionMatchIterator iter = exp.globalMatch(result);
-    int replaceDiff = 0;
-    while (iter.hasNext()) {
-        QRegularExpressionMatch match = iter.next();
-        QString key = match.captured();
-        int startPos = match.capturedStart();
-        int keyLength = key.length();
-        if (emoticonToPath.find(key) != emoticonToPath.end()) {
-            QString imgRichText = getAsRichText(key);
-            result.replace(startPos + replaceDiff, keyLength, imgRichText);
-            replaceDiff += imgRichText.length() - keyLength;
+    int replaceDiff = 0; // how much we have increased message size by replacing emoji with image tags
+    int curCharLength;
+    QStringRef curChar; // the current UTF-32 which we are examining, which may look like a 2-length QString
+    for (int strIndex = 0; strIndex < msg.length(); strIndex += curCharLength) {
+        if ((msg.length() - strIndex >= 2) && ((curChar = msg.midRef(strIndex, 2)).toUcs4().length() == 1)) {
+            // if converting two characters to UTF-32 gives us a single valid character, this is actually a single
+            // 4-byte character, with a QString length of 2
+            curCharLength = 2;
+        }
+        else {
+            curChar = msg.midRef(strIndex, 1);
+            curCharLength = 1;
+        }
+        if (emoticonToPath.find(curChar.toString()) != emoticonToPath.end()) {
+            QString imgRichText = getAsRichText(curChar.toString());
+            result.replace(strIndex + replaceDiff, curCharLength, imgRichText);
+            replaceDiff += imgRichText.length() - curCharLength;
         }
     }
-
     return result;
 }
 

src/widget/emoticonswidget.cpp
@@ -131,7 +131,7 @@ void EmoticonsWidget::onSmileyClicked()
     if (sender) {
         QString sequence =
             sender->property("sequence").toString().replace("&lt;", "<").replace("&gt;", ">");
-        emit insertEmoticon(' ' + sequence + ' ');
+        emit insertEmoticon(sequence);
     }
 }
 

commit e70356f003e49035c9a640210a42cbff03a23e29

fix(chatlog): Match multi-character emoticons again
Fixed after broken in https://github.com/qTox/qTox/pull/4940. Single-character UTF-8 emoji still work without whitespace on either side, but multi-character emoticon patterns like :smile: or :) do require surrounding whitespace, to avoid matching punctuation or HTML tags.

src/persistence/smileypack.cpp
@@ -258,23 +258,25 @@ QString SmileyPack::smileyfied(const QString& msg)
 {
     QMutexLocker locker(&loadingMutex);
     QString result(msg);
-    int replaceDiff = 0; // how much we have increased message size by replacing emoji with image tags
-    int curCharLength;
-    QStringRef curChar; // the current UTF-32 which we are examining, which may look like a 2-length QString
-    for (int strIndex = 0; strIndex < msg.length(); strIndex += curCharLength) {
-        if ((msg.length() - strIndex >= 2) && ((curChar = msg.midRef(strIndex, 2)).toUcs4().length() == 1)) {
-            // if converting two characters to UTF-32 gives us a single valid character, this is actually a single
-            // 4-byte character, with a QString length of 2
-            curCharLength = 2;
+    for ( auto r = emoticonToPath.begin(); r != emoticonToPath.end(); ++r) {
+        QRegularExpression exp;
+        if (r.key().toUcs4().length() == 1) {
+            // UTF-8 emoji
+            exp.setPattern(r.key());
         }
         else {
-            curChar = msg.midRef(strIndex, 1);
-            curCharLength = 1;
+            // pattern like ":)" or ":smile:", don't match inside a word or else will hit punctuation and html tags
+            exp.setPattern(QStringLiteral(R"((?<=^|[\s\n]))") + QRegularExpression::escape(r.key()) + QStringLiteral(R"((?=$|[\s\n]))"));
         }
-        if (emoticonToPath.find(curChar.toString()) != emoticonToPath.end()) {
-            QString imgRichText = getAsRichText(curChar.toString());
-            result.replace(strIndex + replaceDiff, curCharLength, imgRichText);
-            replaceDiff += imgRichText.length() - curCharLength;
+        int replaceDiff = 0;
+        QRegularExpressionMatchIterator iter = exp.globalMatch(result);
+        while (iter.hasNext()) {
+            QRegularExpressionMatch match = iter.next();
+            int startPos = match.capturedStart();
+            int keyLength = r.key().length();
+            QString imgRichText = getAsRichText(r.key());
+            result.replace(startPos + replaceDiff, keyLength, imgRichText);
+            replaceDiff += imgRichText.length() - keyLength;
         }
     }
     return result;