bc773cf7d5c1802481c4cfe3b03baccb8b76522d
tox-user <32248732+tox-user@users.noreply.github.com>
2018-02-27 22:57:14 +0100

    fix(ui): Small fixes to HotkeyInput's style

--- src/widget/form/settings/avform.cpp
+++ src/widget/form/settings/avform.cpp
@@ -576,7 +576,7 @@ void AVForm::showPttShortcutKeys()
         keyString += QString::number(keys[i]) + "+";
     }
 
-    keyString.replace(QRegExp("++$"), "");
+    keyString.replace(QRegExp("[+]+$"), "");
     pushToTalkShortcutInput->setText(keyString);
     pushToTalkShortcutInput->blockSignals(previouslyBlocked);
 }
