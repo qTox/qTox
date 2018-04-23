441931765ffe3de349b28a28bf10a006edcc9949
sudden6 <sudden6@gmx.at>
2018-04-23 19:31:58 +0200

    fix(build): make qTox compile with ffmpeg 4.0 and newer

--- src/video/camerasource.cpp
+++ src/video/camerasource.cpp
@@ -109,7 +109,11 @@ CameraSource::CameraSource()
     moveToThread(deviceThread);
 
     subscriptions = 0;
+
+// TODO(sudden6): remove code when minimum ffmpeg version >= 4.0
+#if LIBAVFORMAT_VERSION_INT < AV_VERSION_INT(58, 9, 100)
     av_register_all();
+#endif
     avdevice_register_all();
 }
 
