1931c28c652bb8699ec124c5e603352c74f9a079
sudden6 <sudden6@gmx.at>
2018-06-17 18:12:09 +0200

    fix(flatpak): compile ffmpeg with the correct options

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -1,27 +1,8 @@
 #!/usr/bin/env bash
 
-# MIT License
+# SPDX-License-Identifier: GPL-3.0+
 #
 # Copyright © 2018 by The qTox Project Contributors
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
 
 # Fail out on error
 set -exuo pipefail
@@ -44,7 +25,7 @@ echo "deb http://ftp.debian.org/debian stretch-backports main" > /etc/apt/source
 
 # Get packages
 apt-get update
-apt-get install $APT_FLAGS ca-certificates git elfutils wget
+apt-get install $APT_FLAGS ca-certificates git elfutils wget xz-utils
 
 # install recent flatpak packages
 apt-get install $APT_FLAGS -t stretch-backports flatpak flatpak-builder
