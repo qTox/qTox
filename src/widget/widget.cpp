9555b9e303863f0bd992d2733c894a55ceab8a91
sudden6 <sudden6@gmx.at>
2018-07-11 20:55:16 +0200

    feat(notify): add desktop notifications using snorenotify
    This commit adds very basic support for desktop notifications on friend
    request, group invites, friend messages and group messages.

--- src/widget/widget.cpp
+++ src/widget/widget.cpp
@@ -1369,6 +1369,9 @@ bool Widget::newFriendMessageAlert(int friendId, bool sound)
         f->setEventFlag(true);
         widget->updateStatusLight();
         ui->friendList->trackWidget(widget);
+#if DESKTOP_NOTIFICATIONS
+        notifier.notifyFriendMessage();
+#endif
 
         if (contentDialog == nullptr) {
             if (hasActive) {
@@ -1406,6 +1409,9 @@ bool Widget::newGroupMessageAlert(int groupId, bool notify)
 
     g->setEventFlag(true);
     widget->updateStatusLight();
+#if DESKTOP_NOTIFICATIONS
+    notifier.notifyGroupMessage();
+#endif
 
     if (contentDialog == nullptr) {
         if (hasActive) {
@@ -1474,6 +1480,9 @@ void Widget::onFriendRequestReceived(const ToxPk& friendPk, const QString& messa
     if (addFriendForm->addFriendRequest(friendPk.toString(), message)) {
         friendRequestsUpdate();
         newMessageAlert(window(), isActiveWindow(), true, true);
+#if DESKTOP_NOTIFICATIONS
+        notifier.notifyFriendRequest();
+#endif
     }
 }
 
@@ -1710,6 +1719,9 @@ void Widget::onGroupInviteReceived(const GroupInvite& inviteInfo)
             ++unreadGroupInvites;
             groupInvitesUpdate();
             newMessageAlert(window(), isActiveWindow(), true, true);
+#if DESKTOP_NOTIFICATIONS
+            notifier.notifyGroupInvite();
+#endif
         }
     } else {
         qWarning() << "onGroupInviteReceived: Unknown groupchat type:" << confType;
