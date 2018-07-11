291e86d9f9ea877fd92daa3b83ac283d56b305d5
sudden6 <sudden6@gmx.at>
2018-07-11 10:31:22 +0200

    fix(ui): don't emit filename change windows for every chat

--- src/core/corefile.cpp
+++ src/core/corefile.cpp
@@ -283,7 +283,7 @@ void CoreFile::removeFile(uint32_t friendId, uint32_t fileId)
 
 QString CoreFile::getCleanFileName(QString filename)
 {
-    QRegularExpression regex("[<>:\"/\\|?*]");
+    QRegularExpression regex{QStringLiteral(R"([<>:"/\\|?])")};
     filename.replace(regex, "_");
 
     return filename;
@@ -295,9 +295,9 @@ void CoreFile::onFileReceiveCallback(Tox*, uint32_t friendId, uint32_t fileId, u
 {
     Core* core = static_cast<Core*>(vCore);
     auto filename = ToxString(fname, fnameLen);
+    const ToxPk friendPk = core->getFriendPublicKey(friendId);
 
     if (kind == TOX_FILE_KIND_AVATAR) {
-        const ToxPk friendPk = core->getFriendPublicKey(friendId);
         if (!filesize) {
             qDebug() << QString("Received empty avatar request %1:%2").arg(friendId).arg(fileId);
             // Avatars of size 0 means explicitely no avatar
@@ -318,7 +318,7 @@ void CoreFile::onFileReceiveCallback(Tox*, uint32_t friendId, uint32_t fileId, u
         if (cleanFileName != filename.getQString()) {
             qDebug() << QStringLiteral("Cleaned filename");
             filename = ToxString(cleanFileName);
-            emit core->fileNameChanged();
+            emit core->fileNameChanged(friendPk);
         } else {
             qDebug() << QStringLiteral("filename already clean");
         }
