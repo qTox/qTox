0f073d47b22bb01a8a95f5e46567c4cb976acbf2
iphydf <iphydf@users.noreply.github.com>
2018-08-26 16:43:38 +0000

    refactor: Improve toxcore-originated log messages.
    This changes ToxLogger to behave as if `qDebug()` were used inside
    toxcore itself, i.e. with the correct file/line/func.

--- src/core/toxlogger.cpp
+++ src/core/toxlogger.cpp
@@ -30,31 +30,39 @@ QString getToxLogLevel(TOX_LOG_LEVEL level) {
     }
 }
 
+static QByteArray cleanPath(const char *file)
+{
+    // for privacy, make the path relative to the c-toxcore source directory
+    const QRegularExpression pathCleaner(QLatin1Literal{"[\\s|\\S]*c-toxcore."});
+    QByteArray cleanedPath = QString{file}.remove(pathCleaner).toUtf8();
+    cleanedPath.append('\0');
+    return cleanedPath;
+}
+
 /**
  * @brief Log message handler for toxcore log messages
  * @note See tox.h for the parameter definitions
  */
 void ToxLogger::onLogMessage(Tox *tox, TOX_LOG_LEVEL level, const char *file, uint32_t line,
-                                    const char *func, const char *message, void *user_data)
+                             const char *func, const char *message, void *user_data)
 {
-    // for privacy, make the path relative to the c-toxcore source directory
-    const QRegularExpression pathCleaner(QLatin1Literal{"[\\s|\\S]*c-toxcore."});
-    const QString cleanPath = QString{file}.remove(pathCleaner);
-
-    const QString logMsg = getToxLogLevel(level) % QLatin1Literal{":"} % cleanPath
-                           % QLatin1Literal{":"} % func % QStringLiteral(":%1: ").arg(line)
-                           % message;
+    const QByteArray cleanedPath = cleanPath(file);
 
     switch (level) {
     case TOX_LOG_LEVEL_TRACE:
         return; // trace level generates too much noise to enable by default
     case TOX_LOG_LEVEL_DEBUG:
+        QMessageLogger(cleanedPath.data(), line, func).debug() << message;
+        break;
     case TOX_LOG_LEVEL_INFO:
-        qDebug() << logMsg;
+        QMessageLogger(cleanedPath.data(), line, func).info() << message;
         break;
     case TOX_LOG_LEVEL_WARNING:
+        QMessageLogger(cleanedPath.data(), line, func).warning() << message;
+        break;
     case TOX_LOG_LEVEL_ERROR:
-        qWarning() << logMsg;
+        QMessageLogger(cleanedPath.data(), line, func).critical() << message;
+        break;
     }
 }
 
