aaf5229ece69fb694381164f10bb85eb6fd53ee3
Monsterovich <monsterovich@gmail.com>
2018-10-22 02:01:05 +0200

    feat(ui): Added feature to generate colors for user names in tox groups

--- src/widget/form/settings/userinterfaceform.cpp
+++ src/widget/form/settings/userinterfaceform.cpp
@@ -73,6 +73,7 @@ UserInterfaceForm::UserInterfaceForm(SettingsWidget* myParent)
     bodyUI->txtChatFont->setCurrentFont(chatBaseFont);
     int index = static_cast<int>(s.getStylePreference());
     bodyUI->textStyleComboBox->setCurrentIndex(index);
+    bodyUI->gcColors->setChecked(s.getEnableGroupChatsColor());
 
     bodyUI->notify->setChecked(s.getNotify());
     // Note: UI is boolean inversed from settings to maintain setting file backwards compatibility
@@ -373,4 +374,9 @@ void UserInterfaceForm::on_txtChatFontSize_valueChanged(int px)
         tmpFont.setPixelSize(px);
         s.setChatMessageFont(tmpFont);
     }
+}
+
+void UserInterfaceForm::on_gcColors_stateChanged(int arg1)
+{
+    Settings::getInstance().setEnableGroupChatsColor(arg1);
 }
