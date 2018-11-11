25b4e766bc53f65defced163e381f16aa4ebf0e8
sudden6 <sudden6@gmx.at>
2018-11-11 12:51:38 +0100

    feat(persistence): add logic to upgrade from one version to the next
    This provides a central place to perform incremental upgrades in our
    persistence layer. At the same time we can keep the code separated from
    the operational code base.

---
+++ src/persistence/upgrader/persistenceupgrader.h
@@ -0,0 +1,31 @@
+#ifndef PERSISTENCEUPGRADER_H
+#define PERSISTENCEUPGRADER_H
+
+#include <QString>
+
+class PersistenceUpgrader
+{
+    static bool runUpgrade(bool portable);
+private:
+    PersistenceUpgrader(bool portable);
+    bool isUpgradeNeeded();
+    bool doUpgrade();
+
+    bool isVersion1_16_3();
+    QString getSettingsPath_1_17_0();
+    QString versionFromFile();
+    QString getSettingsDir_1_16_3() const;
+
+    bool checkedMove(QString source, QString dest);
+
+    QString getProfilesDir_1_17_0() const;
+    QString getGlobalSettingsPath_1_17_0() const;
+    bool upgradeFrom_1_16_3();
+
+private:
+    bool portable;
+    QString curVersion{};
+
+};
+
+#endif // PERSISTENCEUPGRADER_H
