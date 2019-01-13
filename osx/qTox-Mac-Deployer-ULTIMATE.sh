fd7097222d48304e3e509d20488eb158eef9c493
Anthony Bilinski <me@abilinski.com>
2019-01-12 22:54:02 -0800

    fix(build): show brew compilation progress to avoid being killed by Travis

--- osx/qTox-Mac-Deployer-ULTIMATE.sh
+++ osx/qTox-Mac-Deployer-ULTIMATE.sh
@@ -171,7 +171,9 @@ install() {
     # needed for kf5-sonnet
     brew tap kde-mac/kde
 
-    brew install ffmpeg libexif qrencode qt5 sqlcipher openal-soft kf5-sonnet
+    # verbose so that build output is shown, otherwise qt5 build has no output for >10 mins
+    # and is killed by Travis CI
+    brew install --verbose ffmpeg libexif qrencode qt5 sqlcipher openal-soft kf5-sonnet
 
     fcho "Cloning filter_audio ... "
     git clone --branch v0.0.1 --depth=1 https://github.com/irungentoo/filter_audio "$FILTERAUIO_DIR"
