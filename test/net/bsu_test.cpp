e2a5c132ed6db301a71ff075695c56ba184abe11
sudden6 <sudden6@gmx.at>
2019-02-24 21:41:15 +0100

    feat: load bootstrap nodes directly from JSON
    This allows us to easily update the list of bootstrap nodes.

--- test/net/bsu_test.cpp
+++ test/net/bsu_test.cpp
@@ -34,11 +34,14 @@ public:
     TestBootstrapNodesUpdater();
 private slots:
     void testOnline();
+    void testLocal();
 };
 
 TestBootstrapNodesUpdater::TestBootstrapNodesUpdater()
 {
     qRegisterMetaType<QList<DhtServer>>("QList<DhtServer>");
+    // Contains the builtin nodes list
+    Q_INIT_RESOURCE(res);
 }
 
 void TestBootstrapNodesUpdater::testOnline()
@@ -53,6 +56,12 @@ void TestBootstrapNodesUpdater::testOnline()
     QCOMPARE(spy.count(), 1); // make sure the signal was emitted exactly one time
     QList<DhtServer> result = qvariant_cast<QList<DhtServer>>(spy.at(0).at(0));
     QVERIFY(result.size() > 0); // some data should be returned
+}
+
+void TestBootstrapNodesUpdater::testLocal()
+{
+    QList<DhtServer> defaultNodes = BootstrapNodeUpdater::loadDefaultBootstrapNodes();
+    QVERIFY(defaultNodes.size() > 0);
 }
 
 QTEST_GUILESS_MAIN(TestBootstrapNodesUpdater)
