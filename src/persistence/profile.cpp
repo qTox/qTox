b0de2a4ae9edd9dc238239f6c9fcef6a68a455a4
sudden6 <sudden6@gmx.at>
2019-03-03 22:04:33 +0100

    refactor(core): remove unused function
    This function would always return true for any non destroyed Core
    object.

--- src/persistence/profile.cpp
+++ src/persistence/profile.cpp
@@ -241,17 +241,15 @@ Profile* Profile::createProfile(QString name, QString password)
 
 Profile::~Profile()
 {
-    if (!isRemoved && core->isReady()) {
-        onSaveToxSave();
+    if (isRemoved) {
+        return;
     }
 
-    if (!isRemoved) {
-        Settings::getInstance().savePersonal(this);
-        Settings::getInstance().sync();
-        ProfileLocker::assertLock();
-        assert(ProfileLocker::getCurLockName() == name);
-        ProfileLocker::unlock();
-    }
+    Settings::getInstance().savePersonal(this);
+    Settings::getInstance().sync();
+    ProfileLocker::assertLock();
+    assert(ProfileLocker::getCurLockName() == name);
+    ProfileLocker::unlock();
 }
 
 /**
@@ -332,7 +330,6 @@ void Profile::startCore()
  */
 void Profile::onSaveToxSave()
 {
-    assert(core->isReady());
     QByteArray data = core->getToxSaveData();
     assert(data.size());
     saveToxSave(data);
