516c52ad207e84fa88606170fdf8d8e94872314c
Maxim Biro <nurupo.contributions@gmail.com>
2019-05-26 10:52:09 -0400

    feat(travis): publish nightly builds off Travis-CI

---
+++ .travis/cirp/update_cache.sh
@@ -0,0 +1,8 @@
+if [ ! -z "$TRAVIS_TEST_RESULT" ] && [ "$TRAVIS_TEST_RESULT" != "0" ]
+then
+  echo "Build has failed, skipping updating the cache"
+  exit 0
+fi
+
+mkdir -p /opt/cirp
+git rev-parse HEAD > /opt/cirp/previous_runs_commit
