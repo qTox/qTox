8832234758d5a025afa15514f0526d3915e1460f
jenli669 <lindgren.jens@gmail.com>
2019-08-12 23:07:34 +0200

    feat(login): generate a GUI error when auto login fails

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,7 @@
 #include <QMutex>
 #include <QMutexLocker>
 
+#include <QtWidgets/QMessageBox>
 #include <ctime>
 #include <sodium.h>
 #include <stdio.h>
@@ -343,6 +344,10 @@ int main(int argc, char* argv[])
     Profile* profile = nullptr;
     if (autoLogin && Profile::exists(profileName) && !Profile::isEncrypted(profileName)) {
         profile = Profile::loadProfile(profileName, QString(), settings);
+        if (!profile) {
+            QMessageBox::information(nullptr, QObject::tr("Error"),
+                                     QObject::tr("Failed to load profile automatically."));
+        }
     }
     if (profile) {
         settings.updateProfileData(profile);
