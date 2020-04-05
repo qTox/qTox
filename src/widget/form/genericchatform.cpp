e3e763f30428c707e087f7e7d2f7c150ad56ee28
Anthony Bilinski <me@abilinski.com>
2020-04-04 22:39:02 -0700

    revert(chatlog): partially revert "prevent invalid history access"
    This partially reverts commit e3e6e1d9c4e22d6f090f153628677ad427cf4900.

--- src/widget/form/genericchatform.cpp
+++ src/widget/form/genericchatform.cpp
@@ -959,10 +959,6 @@ void GenericChatForm::searchInBegin(const QString& phrase, const ParameterSearch
         return;
     }
 
-    if (messages.size() == 0) {
-        return;
-    }
-
     if (chatLog.getNextIdx() == messages.rbegin()->first + 1) {
         disableSearchText();
     } else {
