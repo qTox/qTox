83f543fab8e75a3134f7fe779bdf8fa6e6b37427
Maxim Biro <nurupo.contributions@gmail.com>
2020-06-06 15:15:12 -0400

    chore(build): Update copyright year
    (cherry picked from commit 2f6d3f443fd2e825a4145b398834b20c5bc1cfbc)

--- windows/cross-compile/build.sh
+++ windows/cross-compile/build.sh
@@ -2,7 +2,7 @@
 
 # MIT License
 #
-# Copyright (c) 2017-2018 Maxim Biro <nurupo.contributions@gmail.com>
+# Copyright (c) 2017-2020 Maxim Biro <nurupo.contributions@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
