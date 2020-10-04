e37a8cf82fef4e362bb3ed8e0c37a77eab30d741
Maxim Biro <nurupo.contributions@gmail.com>
2020-10-04 08:32:47 -0400

    chore(windows): update Qt to 5.12.9
    (cherry picked from commit dc78f410f0901ae45c974aed777cccac39eef900)

--- windows/cross-compile/build.sh
+++ windows/cross-compile/build.sh
@@ -274,10 +274,10 @@ fi
 QT_PREFIX_DIR="$DEP_DIR/libqt5"
 QT_MAJOR=5
 QT_MINOR=12
-QT_PATCH=8
+QT_PATCH=9
 QT_VERSION=$QT_MAJOR.$QT_MINOR.$QT_PATCH
-# hash from https://download.qt.io/archive/qt/5.12/5.12.8/single/qt-everywhere-src-5.12.8.tar.xz.mirrorlist
-QT_HASH="9142300dfbd641ebdea853546511a352e4bd547c4c7f25d61a40cd997af1f0cf"
+# hash from https://download.qt.io/archive/qt/5.12/5.12.9/single/qt-everywhere-src-5.12.9.tar.xz.mirrorlist
+QT_HASH="a628186814b73e93594ee8e72f975116599f946919ae6bd86611981b739acff0"
 QT_FILENAME="qt-everywhere-src-$QT_VERSION.tar.xz"
 if [ ! -f "$QT_PREFIX_DIR/done" ]
 then
