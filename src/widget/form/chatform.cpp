d338ed97d95948b21c141ddefdc09039e14bdaa0
Mick Sayson <mick@sayson.com>
2021-01-30 17:35:21 -0800

    refactor(chatform): Move typing notification creation into ChatLog
    * Part of attempt to reduce interdependencies between
      ChatForm<->ChatLog.

--- src/widget/form/chatform.cpp
+++ src/widget/form/chatform.cpp
@@ -126,7 +126,6 @@ ChatForm::ChatForm(Profile& profile, Friend* chatFriend, IChatLog& chatLog, IMes
 
     callDurationTimer = nullptr;
 
-    chatWidget->setTypingNotification(ChatMessage::createTypingNotification());
     chatWidget->setMinimumHeight(CHAT_WIDGET_MIN_HEIGHT);
 
     callDuration = new QLabel();
@@ -673,10 +672,8 @@ void ChatForm::onUpdateTime()
 void ChatForm::setFriendTyping(bool isTyping)
 {
     chatWidget->setTypingNotificationVisible(isTyping);
-    Text* text = static_cast<Text*>(chatWidget->getTypingNotification()->getContent(1));
-    QString typingDiv = "<div class=typing>%1</div>";
     QString name = f->getDisplayedName();
-    text->setText(typingDiv.arg(tr("%1 is typing").arg(name)));
+    chatWidget->setTypingNotificationName(name);
 }
 
 void ChatForm::show(ContentLayout* contentLayout)
@@ -686,7 +683,6 @@ void ChatForm::show(ContentLayout* contentLayout)
 
 void ChatForm::reloadTheme()
 {
-    chatWidget->setTypingNotification(ChatMessage::createTypingNotification());
     GenericChatForm::reloadTheme();
 }
 
