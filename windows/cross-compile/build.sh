057f921ace8894f7c193f8504bc44bdb78488fce
Anthony Bilinski <me@abilinski.com>
2021-10-24 04:53:56 -0700

    fix(build): Add std threading support in Windows build
    Use posix versions of mingw toolchains, which say:
    > This package contains the C++ compiler, supporting cross-compiling to
    64-bit MinGW-w64 targets, using the POSIX threading model.
    
    Fix build failure saying that std::mutex was not declared

--- windows/cross-compile/build.sh
+++ windows/cross-compile/build.sh
@@ -152,11 +152,15 @@ then
   apt-get install -y --no-install-recommends \
                     g++-mingw-w64-i686 \
                     gcc-mingw-w64-i686
+  update-alternatives --set i686-w64-mingw32-gcc /usr/bin/i686-w64-mingw32-gcc-posix
+  update-alternatives --set i686-w64-mingw32-g++ /usr/bin/i686-w64-mingw32-g++-posix
 elif [[ "$ARCH" == "x86_64" ]]
 then
   apt-get install -y --no-install-recommends \
                     g++-mingw-w64-x86-64 \
                     gcc-mingw-w64-x86-64
+  update-alternatives --set x86_64-w64-mingw32-gcc /usr/bin/x86_64-w64-mingw32-gcc-posix
+  update-alternatives --set x86_64-w64-mingw32-g++ /usr/bin/x86_64-w64-mingw32-g++-posix
 fi
 
 
