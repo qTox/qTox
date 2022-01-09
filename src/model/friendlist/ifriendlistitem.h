400849723d8d25c4b30bd6d862d9c9cfbc3942d9
bodwok <ewgsupp@gmail.com>
2022-01-09 20:52:42 +0300

    test(FriendListManager): add contact management test

--- src/model/friendlist/ifriendlistitem.h
+++ src/model/friendlist/ifriendlistitem.h
@@ -36,9 +36,11 @@ public:
     virtual bool isFriend() const = 0;
     virtual bool isGroup() const = 0;
     virtual bool isOnline() const = 0;
+    virtual bool widgetIsVisible() const = 0;
     virtual QString getNameItem() const = 0;
     virtual QDateTime getLastActivity() const = 0;
     virtual QWidget* getWidget() = 0;
+    virtual void setWidgetVisible(bool) = 0;
 
     virtual int getCircleId() const
     {
