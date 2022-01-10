88edc66f88bb2eaaeda156f55a0731999bd81541
Mick Sayson <mick@sayson.com>
2022-01-09 18:46:52 -0800

    fix(buildscripts): Fix license text for MIT derived works

--- buildscripts/build_aitool_linux.sh
+++ buildscripts/build_aitool_linux.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-#    Copyright © 2021 by The qTox Project Contributors
-#
-#    This program is libre software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# SPDX-License-Identifier: GPL-3.0-or-later AND MIT
+#    Copyright © 2019-2021 by The qTox Project Contributors
 
 set -euo pipefail
 
