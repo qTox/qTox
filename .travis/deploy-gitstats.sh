5a77a18fc09f8af5ffa7bc7a949eea505abe989b
Anthony Bilinski <me@abilinski.com>
2022-02-16 08:55:08 -0800

    fix(gitstats): Re-enable deploying of gitstats to qtox.github.io
    Hasn't been run since the travis days.
    
    Now managed through a deploy key rather than an access token to limit scope to
    a single repo. Private key is stored in qTox/qTox as a repository secret, and
    the public portion is saved to qTox/gitstats as a deploy key.

--- .travis/deploy-gitstats.sh
+++ .travis/deploy-gitstats.sh
@@ -26,4 +26,7 @@ git add .
 git commit --quiet -m "Deploy to GH pages from commit: $COMMIT"
 
 echo "Pushing to GH pages..."
-git push --force --quiet "https://${GH_TOKEN_GITSTATS}@github.com/qTox/gitstats.git" master:gh-pages &> /dev/null
+touch /tmp/access_key
+chmod 600 /tmp/access_key
+echo "$access_key" > /tmp/access_key
+GIT_SSH_COMMAND="ssh -i /tmp/access_key" git push --force --quiet "git@github.com/qTox/gitstats.git" master:gh-pages
