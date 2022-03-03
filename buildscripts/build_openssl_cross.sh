11767f541fa1134b667bd4d208c3f96ec2655928
Anthony Bilinski <me@abilinski.com>
2022-03-03 02:39:34 -0800

    chore(CI): Add support for building openssl for macOS

--- buildscripts/build_openssl_cross.sh
+++ buildscripts/build_openssl_cross.sh
@@ -22,18 +22,26 @@ parse_arch "$@"
 
 if [[ "$ARCH" == "x86_64" ]]; then
     OPENSSL_ARCH="mingw64"
+    CROSS_COMPILE_ARCH="--cross-compile-prefix=${ARCH}-w64-mingw32-"
 elif [[ "$ARCH" == "i686" ]]; then
     OPENSSL_ARCH="mingw"
+    CROSS_COMPILE_ARCH="--cross-compile-prefix=${ARCH}-w64-mingw32-"
+elif [[ "$ARCH" == "macos" ]]; then
+    OPENSSL_ARCH="darwin64-x86_64-cc"
+    CROSS_COMPILE_ARCH=""
 else
     echo "Invalid architecture"
     exit 1
 fi
 
-./Configure "--prefix=${DEP_PREFIX}" \
+CFLAGS="${CROSS_CFLAG}" \
+LDFLAGS="${CROSS_LDFLAG}" \
+./Configure \
+    "--prefix=${DEP_PREFIX}" \
     "--openssldir=${DEP_PREFIX}/ssl" \
     shared \
-    $OPENSSL_ARCH \
-    "${CROSS_COMPILE_ARCH}"
+    ${CROSS_COMPILE_ARCH} \
+    "${OPENSSL_ARCH}" \
 
 make -j "${MAKE_JOBS}"
 make install
