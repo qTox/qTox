8fec10afb7df88cf68f62d3c1557b7438983f346
Anthony Bilinski <me@abilinski.com>
2022-03-03 01:33:17 -0800

    chore(CI): Move qrencode build from autotools to cmake
    Autotools build was broken on macOS, and README mentions "If the configure
    script does not work well, try to use CMake."

--- buildscripts/build_qrencode_cross.sh
+++ buildscripts/build_qrencode_cross.sh
@@ -30,13 +30,12 @@ fi
 
 "$(dirname "$0")"/download/download_qrencode.sh
 
-CFLAGS="-O2 -g0" ./configure --host="${ARCH}-w64-mingw32" \
-                            --prefix=/windows \
-                            --enable-shared \
-                            --disable-static \
-                            --disable-sdltest \
-                            --without-tools \
-                            --without-debug
+cmake . \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/windows \
+    "${CMAKE_TOOLCHAIN_FILE}" \
+    -DWITH_TOOLS=OFF \
+    -DBUILD_SHARED_LIBS=ON
 
 make -j $(nproc)
 make install
