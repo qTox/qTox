ad862ca50e675e05c2338cd02e47a5df732c294e
Anthony Bilinski <me@abilinski.com>
2022-03-03 02:39:34 -0800

    chore(CI): Add support for building openssl for macOS

--- buildscripts/build_openssl.sh
+++ buildscripts/build_openssl.sh
@@ -10,21 +10,29 @@ readonly SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 source "${SCRIPT_DIR}/build_utils.sh"
 
-parse_arch --dep "openssl" --supported "win32 win64" "$@"
+parse_arch --dep "openssl" --supported "win32 win64 macos" "$@"
 
 if [[ "$SCRIPT_ARCH" == "win64" ]]; then
     OPENSSL_ARCH="mingw64"
-else
+    CROSS_COMPILE_ARCH="--cross-compile-prefix=${MINGW_ARCH}-w64-mingw32-"
+elif [[ "$SCRIPT_ARCH" == "win32" ]]; then
     OPENSSL_ARCH="mingw"
+    CROSS_COMPILE_ARCH="--cross-compile-prefix=${MINGW_ARCH}-w64-mingw32-"
+elif [[ "$SCRIPT_ARCH" == "macos" ]]; then
+    OPENSSL_ARCH="darwin64-x86_64-cc"
+    CROSS_COMPILE_ARCH=""
 fi
 
 "${SCRIPT_DIR}/download/download_openssl.sh"
 
-./Configure "--prefix=${DEP_PREFIX}" \
+CFLAGS="${CROSS_CFLAG}" \
+LDFLAGS="${CROSS_LDFLAG}" \
+./Configure \
+    "--prefix=${DEP_PREFIX}" \
     "--openssldir=${DEP_PREFIX}/ssl" \
     shared \
-    $OPENSSL_ARCH \
-    "--cross-compile-prefix=${MINGW_ARCH}-w64-mingw32-"
+    ${CROSS_COMPILE_ARCH} \
+    "${OPENSSL_ARCH}" \
 
 make -j "${MAKE_JOBS}"
 make install_sw
