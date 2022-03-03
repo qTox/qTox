d3bebab0b7a66e2825a1c4087577ea5d69c6a454
Anthony Bilinski <me@abilinski.com>
2022-03-03 02:25:04 -0800

    chore(CI): Add support for building sqlcipher for macOS

--- buildscripts/build_sqlcipher.sh
+++ buildscripts/build_sqlcipher.sh
@@ -10,23 +10,34 @@ readonly SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 source "${SCRIPT_DIR}/build_utils.sh"
 
-parse_arch --dep "sqlcipher" --supported "win32 win64" "$@"
+parse_arch --dep "sqlcipher" --supported "win32 win64 macos" "$@"
 
 "${SCRIPT_DIR}/download/download_sqlcipher.sh"
 
-sed -i s/'if test "$TARGET_EXEEXT" = ".exe"'/'if test ".exe" = ".exe"'/g configure
-sed -i 's|exec $PWD/mksourceid manifest|exec $PWD/mksourceid.exe manifest|g' tool/mksqlite3h.tcl
+CFLAGS="-O2 -g0 -DSQLITE_HAS_CODEC -I$DEP_PREFIX/include/ $CROSS_CFLAG"
+LDFLAGS="-lcrypto -L$DEP_PREFIX/lib/ $CROSS_LDFLAG"
+
+if [ "${SCRIPT_ARCH}" == "macos" ]; then
+    LIBS=''
+else
+    sed -i s/'if test "$TARGET_EXEEXT" = ".exe"'/'if test ".exe" = ".exe"'/g configure
+    sed -i 's|exec $PWD/mksourceid manifest|exec $PWD/mksourceid.exe manifest|g' tool/mksqlite3h.tcl
+    LIBS="-lgdi32 -lws2_32"
+    LDFLAGS="$LDFLAGS -lgdi32"
+fi
 
 ./configure "${HOST_OPTION}" \
             "--prefix=${DEP_PREFIX}" \
             --enable-shared \
             --disable-static \
             --enable-tempstore=yes \
-            CFLAGS="-O2 -g0 -DSQLITE_HAS_CODEC -I${DEP_PREFIX}/include/" \
-            LDFLAGS="-lcrypto -lgdi32 -L${DEP_PREFIX}/lib/" \
-            LIBS="-lgdi32 -lws2_32"
+            "CFLAGS=${CFLAGS} ${CROSS_CFLAG}" \
+            "LDFLAGS=${LDFLAGS} ${CROSS_LDFLAG}" \
+            "LIBS=${LIBS}"
 
-sed -i s/"TEXE = $"/"TEXE = .exe"/ Makefile
+if [ "${SCRIPT_ARCH}" != "macos" ]; then
+    sed -i s/"TEXE = $"/"TEXE = .exe"/ Makefile
+fi
 
 make -j "${MAKE_JOBS}"
 make install
