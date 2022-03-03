d674815bb5e11c4e1b321e53f75b2ed66e931771
Anthony Bilinski <me@abilinski.com>
2022-03-03 02:25:04 -0800

    chore(CI): Add support for building sqlcipher for macOS

--- buildscripts/build_sqlcipher.sh
+++ buildscripts/build_sqlcipher.sh
@@ -12,27 +12,38 @@ source "${SCRIPT_DIR}/cross_compile_detection.sh"
 
 usage()
 {
-    echo "Download and build sqlcipher for the windows cross compiling environment"
-    echo "Usage: $0 --arch {winx86_64|wini686}"
+    echo "Download and build sqlcipher for Windows or macOS"
+    echo "Usage: $0 --arch {winx86_64|wini686|macos}"
 }
 
 parse_arch "$@"
 
 "${SCRIPT_DIR}/download/download_sqlcipher.sh"
 
-sed -i s/'if test "$TARGET_EXEEXT" = ".exe"'/'if test ".exe" = ".exe"'/g configure
-sed -i 's|exec $PWD/mksourceid manifest|exec $PWD/mksourceid.exe manifest|g' tool/mksqlite3h.tcl
+CFLAGS="-O2 -g0 -DSQLITE_HAS_CODEC -I$DEP_PREFIX/include/ $CROSS_CFLAG"
+LDFLAGS="-lcrypto -L$DEP_PREFIX/lib/ $CROSS_LDFLAG"
+
+if [ "$ARCH" == "macos" ]; then
+    LIBS=''
+else
+    sed -i s/'if test "$TARGET_EXEEXT" = ".exe"'/'if test ".exe" = ".exe"'/g configure
+    sed -i 's|exec $PWD/mksourceid manifest|exec $PWD/mksourceid.exe manifest|g' tool/mksqlite3h.tcl
+    LIBS="-lgdi32 -lws2_32"
+    LDFLAGS="$LDFLAGS -lgdi32"
+fi
 
 ./configure "${HOST_OPTION}" \
             "--prefix=${DEP_PREFIX}" \
             --enable-shared \
             --disable-static \
             --enable-tempstore=yes \
-            CFLAGS="-O2 -g0 -DSQLITE_HAS_CODEC -I${DEP_PREFIX}/include/" \
-            LDFLAGS="-lcrypto -lgdi32 -L${DEP_PREFIX}/lib/" \
-            LIBS="-lgdi32 -lws2_32"
+            "CFLAGS=${CFLAGS}" \
+            "LDFLAGS=${LDFLAGS}" \
+            "LIBS=${LIBS}"
 
-sed -i s/"TEXE = $"/"TEXE = .exe"/ Makefile
+if [ "${ARCH}" != "macos" ]; then
+    sed -i s/"TEXE = $"/"TEXE = .exe"/ Makefile
+fi
 
 make -j "${MAKE_JOBS}"
 make install
