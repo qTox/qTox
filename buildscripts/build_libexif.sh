a94304148ef1221f9b72680cfc5699398a9f3c3b
Anthony Bilinski <me@abilinski.com>
2022-03-03 02:51:08 -0800

    chore(CI): Add support for building libexif for macOS

--- buildscripts/build_libexif.sh
+++ buildscripts/build_libexif.sh
@@ -10,11 +10,13 @@ readonly SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 source "${SCRIPT_DIR}/build_utils.sh"
 
-parse_arch --dep "libexif" --supported "win32 win64" "$@"
+parse_arch --dep "libexif" --supported "win32 win64 macos" "$@"
 
 "${SCRIPT_DIR}/download/download_libexif.sh"
 
-CFLAGS="-O2 -g0" ./configure "${HOST_OPTION}" \
+CFLAGS="-O2 -g0 ${CROSS_CFLAG}" \
+LDFLAGS="${CROSS_LDFLAG}" \
+./configure "${HOST_OPTION}" \
                          "--prefix=${DEP_PREFIX}" \
                          --enable-shared \
                          --disable-static \
