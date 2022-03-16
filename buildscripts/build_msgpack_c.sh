e6075c278d3fafa404a5188b95be912357cc3959
Anthony Bilinski <me@abilinski.com>
2022-03-16 01:57:39 -0700

    chore(CI): Add support for building msgpack-c for macOS
    CMAKE_OSX_DEPLOYMENT_TARGET has no effect when not building for macOS.

--- buildscripts/build_msgpack_c.sh
+++ buildscripts/build_msgpack_c.sh
@@ -9,7 +9,7 @@ readonly SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 source "${SCRIPT_DIR}/build_utils.sh"
 
-parse_arch --dep "msgpack-c" --supported "win32 win64" "$@"
+parse_arch --dep "msgpack-c" --supported "win32 win64 macos" "$@"
 
 "${SCRIPT_DIR}/download/download_msgpack_c.sh"
 
@@ -19,6 +19,7 @@ cmake .\
     "${CMAKE_TOOLCHAIN_FILE}" \
     -DMSGPACK_BUILD_EXAMPLES=OFF \
     -DMSGPACK_BUILD_TESTS=OFF \
+    "-DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOS_MINIMUM_SUPPORTED_VERSION}" \
     .
 
 make -j "${MAKE_JOBS}"
