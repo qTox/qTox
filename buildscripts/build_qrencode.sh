02f384a7d29174cc261a7253abb4a2f8e408b4e1
Anthony Bilinski <me@abilinski.com>
2022-03-16 01:58:03 -0700

    chore(CI): Add support for building qrencode for macOS
    CMAKE_OSX_DEPLOYMENT_TARGET has no effect when not building for macOS.

--- buildscripts/build_qrencode.sh
+++ buildscripts/build_qrencode.sh
@@ -10,13 +10,14 @@ readonly SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 source "${SCRIPT_DIR}/build_utils.sh"
 
-parse_arch --dep "qrencode" --supported "win32 win64" "$@"
+parse_arch --dep "qrencode" --supported "win32 win64 macos" "$@"
 
 "${SCRIPT_DIR}/download/download_qrencode.sh"
 
 cmake . \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX="${DEP_PREFIX}" \
+    "-DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOS_MINIMUM_SUPPORTED_VERSION}" \
     "${CMAKE_TOOLCHAIN_FILE}" \
     -DWITH_TOOLS=OFF \
     -DBUILD_SHARED_LIBS=ON
