988288f9c693f5235bc9fda89bbb2bdcca65389b
Anthony Bilinski <me@abilinski.com>
2022-03-25 02:25:46 -0700

    refactor(tests): Move database test utilities to their own lib
    So that we can have multiple db upgrade tests that can run in parallel,
    instead of one monster test that runs independent test cases sequentially.

---
+++ test/dbutility/include/dbutility/dbutility.h
@@ -0,0 +1,50 @@
+/*
+    Copyright © 2022 by The qTox Project Contributors
+
+    This file is part of qTox, a Qt-based graphical interface for Tox.
+
+    qTox is libre software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    qTox is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with qTox.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <QString>
+
+#include <array>
+#include <memory>
+#include <vector>
+
+class RawDatabase;
+
+namespace DbUtility
+{
+    struct SqliteMasterEntry {
+        QString name;
+        QString sql;
+        bool operator==(const DbUtility::SqliteMasterEntry& rhs) const;
+    };
+
+    extern const std::array<QString, 10> testFileList;
+    extern const std::vector<SqliteMasterEntry> schema0;
+    extern const std::vector<SqliteMasterEntry> schema1;
+    extern const std::vector<SqliteMasterEntry> schema2;
+    extern const std::vector<SqliteMasterEntry> schema3;
+    extern const std::vector<SqliteMasterEntry> schema4;
+    extern const std::vector<SqliteMasterEntry> schema5;
+    extern const std::vector<SqliteMasterEntry> schema6;
+    extern const std::vector<SqliteMasterEntry> schema7;
+
+    void createSchemaAtVersion(std::shared_ptr<RawDatabase> db, const std::vector<DbUtility::SqliteMasterEntry>& schema);
+    void verifyDb(std::shared_ptr<RawDatabase> db, const std::vector<DbUtility::SqliteMasterEntry>& expectedSql);
+}
