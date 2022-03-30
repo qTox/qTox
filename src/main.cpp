qTox/qTox

commit 0c967725df8a3f55474c59c48a6f40997c1b974e

refactor: Remove Settings singleton
* Make main.cpp's toxSave free functions into a ToxSave class so that it can be
  given Settings on construction.
* Add void* to IPC callbacks so that classes can get back to themselves.

src/main.cpp
@@ -55,19 +55,23 @@ QList<QByteArray>* logBuffer =
 QMutex* logBufferMutex = new QMutex();
 #endif
 
+std::unique_ptr<Settings> settings;
+std::unique_ptr<ToxSave> toxSave;
+
 void cleanup()
 {
     // force save early even though destruction saves, because Windows OS will
     // close qTox before cleanup() is finished if logging out or shutting down,
     // once the top level window has exited, which occurs in ~Widget within
     // ~Nexus. Re-ordering Nexus destruction is not trivial.
-    auto& s = Settings::getInstance();
-    s.saveGlobal();
-    s.savePersonal();
-    s.sync();
+    if (settings) {
+        settings->saveGlobal();
+        settings->savePersonal();
+        settings->sync();
+    }
 
     Nexus::destroyInstance();
-    Settings::destroyInstance();
+    settings.reset();
     qDebug() << "Cleanup success";
 
 #ifdef LOG_TO_FILE
@@ -183,8 +187,9 @@ void logMessageHandler(QtMsgType type, const QMessageLogContext& ctxt, const QSt
 
 std::unique_ptr<ToxURIDialog> uriDialog;
 
-bool toxURIEventHandler(const QByteArray& eventData)
+bool toxURIEventHandler(const QByteArray& eventData, void* userData)
 {
+    std::ignore = userData;
     if (!eventData.startsWith("tox:")) {
         return false;
     }
@@ -196,7 +201,8 @@ bool toxURIEventHandler(const QByteArray& eventData)
     uriDialog->handleToxURI(eventData);
     return true;
 }
-}
+
+} // namespace
 
 int main(int argc, char* argv[])
 {
@@ -230,8 +236,9 @@ int main(int argc, char* argv[])
         qWarning() << "Couldn't load font";
     }
 
-    Settings& settings = Settings::getInstance();
-    QString locale = settings.getTranslation();
+    settings = std::unique_ptr<Settings>(new Settings());
+
+    QString locale = settings->getTranslation();
     // We need to init the resources in the translations_library explicitely.
     // See https://doc.qt.io/qt-5/resources.html#using-resources-in-a-library
     Q_INIT_RESOURCE(translations);
@@ -272,10 +279,10 @@ int main(int argc, char* argv[])
                                         QObject::tr("(SOCKS5/HTTP/NONE):(ADDRESS):(PORT)")));
     parser.process(*a);
 
-    uint32_t profileId = settings.getCurrentProfileId();
+    uint32_t profileId = settings->getCurrentProfileId();
     IPC ipc(profileId);
     if (ipc.isAttached()) {
-        QObject::connect(&settings, &Settings::currentProfileIdChanged, &ipc, &IPC::setProfileId);
+        QObject::connect(settings.get(), &Settings::currentProfileIdChanged, &ipc, &IPC::setProfileId);
     } else {
         qWarning() << "Can't init IPC, maybe we're in a jail? Continuing with reduced multi-client functionality.";
     }
@@ -287,7 +294,7 @@ int main(int argc, char* argv[])
     }
 
 #ifdef LOG_TO_FILE
-    QString logFileDir = settings.getPaths().getAppCacheDirPath();
+    QString logFileDir = settings->getPaths().getAppCacheDirPath();
     QDir(logFileDir).mkpath(".");
 
     QString logfile = logFileDir + "qtox.log";
@@ -333,14 +340,14 @@ int main(int argc, char* argv[])
     qDebug() << "commit: " << GIT_VERSION;
 
     QString profileName;
-    bool autoLogin = settings.getAutoLogin();
+    bool autoLogin = settings->getAutoLogin();
 
     uint32_t ipcDest = 0;
     bool doIpc = ipc.isAttached();
     QString eventType, firstParam;
     if (parser.isSet("p")) {
         profileName = parser.value("p");
-        if (!Profile::exists(profileName)) {
+        if (!Profile::exists(profileName, settings->getPaths())) {
             qWarning() << "-p profile" << profileName + ".tox"
                        << "doesn't exist, opening login screen";
             doIpc = false;
@@ -353,7 +360,7 @@ int main(int argc, char* argv[])
         doIpc = false;
         autoLogin = false;
     } else {
-        profileName = settings.getCurrentProfile();
+        profileName = settings->getCurrentProfile();
     }
 
     if (parser.positionalArguments().empty()) {
@@ -398,14 +405,14 @@ int main(int argc, char* argv[])
     // TODO(kriby): Consider moving application initializing variables into a globalSettings object
     //  note: Because Settings is shouldering global settings as well as model specific ones it
     //  cannot be integrated into a central model object yet
-    nexus.setSettings(&settings);
+    nexus.setSettings(settings.get());
     auto& cameraSource = Nexus::getCameraSource();
     // Autologin
     // TODO (kriby): Shift responsibility of linking views to model objects from nexus
     // Further: generate view instances separately (loginScreen, mainGUI, audio)
     Profile* profile = nullptr;
-    if (autoLogin && Profile::exists(profileName) && !Profile::isEncrypted(profileName)) {
-        profile = Profile::loadProfile(profileName, QString(), settings, &parser, cameraSource);
+    if (autoLogin && Profile::exists(profileName, settings->getPaths()) && !Profile::isEncrypted(profileName, settings->getPaths())) {
+        profile = Profile::loadProfile(profileName, QString(), *settings, &parser, cameraSource);
         if (!profile) {
             QMessageBox::information(nullptr, QObject::tr("Error"),
                                      QObject::tr("Failed to load profile automatically."));
@@ -426,16 +433,16 @@ int main(int argc, char* argv[])
 
     if (ipc.isAttached()) {
         // Start to accept Inter-process communication
-        ipc.registerEventHandler("uri", &toxURIEventHandler);
-        ipc.registerEventHandler("save", &toxSaveEventHandler);
-        ipc.registerEventHandler("activate", &toxActivateEventHandler);
+        ipc.registerEventHandler("uri", &toxURIEventHandler, uriDialog.get());
+        ipc.registerEventHandler("save", &ToxSave::toxSaveEventHandler, toxSave.get());
+        ipc.registerEventHandler("activate", &toxActivateEventHandler, nullptr);
     }
 
     // Event was not handled by already running instance therefore we handle it ourselves
     if (eventType == "uri") {
         uriDialog->handleToxURI(firstParam.toUtf8());
     } else if (eventType == "save") {
-        handleToxSave(firstParam.toUtf8());
+        toxSave->handleToxSave(firstParam.toUtf8());
     }
 
     QObject::connect(a.get(), &QApplication::aboutToQuit, cleanup);

commit 7565f8b350c54347e3c78a91d05bf50dff1ffe3c

fix(main): Construct ToxSave event handler
Bug introduced in 0c967725df8a3f55474c59c48a6f40997c1b974e, when the class
was added but never constructed.

src/main.cpp
@@ -434,6 +434,7 @@ int main(int argc, char* argv[])
     }
 
     uriDialog = std::unique_ptr<ToxURIDialog>(new ToxURIDialog(nullptr, profile->getCore(), *messageBoxManager));
+    toxSave = std::unique_ptr<ToxSave>(new ToxSave{*settings});
 
     if (ipc.isAttached()) {
         // Start to accept Inter-process communication