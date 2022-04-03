e48c557387f75b12cfcccc5b3c7806e9a8a113fa
Anthony Bilinski <me@abilinski.com>
2022-04-02 19:37:00 -0700

    fix(CI): Fix commit message check on single commit PRs
    The range of COMMIT..COMMIT does not count the starting commit, so
    single commit PRs are completely unchecked, and multi commit PRs don't
    have their first commit checked.

--- .ci-scripts/verify-commit-format.sh
+++ .ci-scripts/verify-commit-format.sh
@@ -21,5 +21,5 @@ set -eu -o pipefail
 
 # Verify commit messages
 readarray -t COMMITS <<<$(curl -s ${GITHUB_CONTEXT} | jq -r '.[0,-1].sha')
-COMMIT_RANGE="${COMMITS[1]}..${COMMITS[0]}"
+COMMIT_RANGE="${COMMITS[1]}~1..${COMMITS[0]}"
 bash ./verify-commit-messages.sh "$COMMIT_RANGE"
