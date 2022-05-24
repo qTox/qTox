60f8ae49027c892b8f36936c312959e9f60f2359
無情天 <kofzhanganguo@126.com>
2022-05-24 13:17:16 +0200

    feat(l10n): update Chinese (Simplified) translation from Weblate

--- translations/zh_CN.ts
+++ translations/zh_CN.ts
@@ -101,7 +101,7 @@ which may lead to problems with video calls.</source>
     </message>
     <message>
         <source>Use slider to set the volume of your speakers.</source>
-        <translation>使用滑块设置扬声器的音量。</translation>
+        <translation>拖动滑块调整音量.</translation>
     </message>
     <message>
         <source>Transmitted audio quality. Lower this setting if your bandwidth is not high enough or if you want to reduce bandwidth usage.</source>
@@ -465,7 +465,7 @@ which may lead to problems with video calls.</source>
     </message>
     <message>
         <source>Unless you %1 know what you are doing, please do %2 change anything here. Changes made here may lead to problems with qTox, and even to loss of your data, e.g. history.%3</source>
-        <translation>除非您%1知道自己在做什么，否则请%2改变这里的任何设置。此处所做的更改可能导致 qTox 出现问题，甚至导致数据丢失，比如聊天历史记录丢失。%3</translation>
+        <translation>除非您 %1 知道自己在做什么，否则请 %2 改变这里的任何设置。此处所做的更改可能导致 qTox 出现问题，甚至导致数据丢失，比如聊天历史记录丢失。%3</translation>
     </message>
     <message>
         <source>really</source>
@@ -2491,11 +2491,11 @@ This ID includes the NoSpam code (in blue), and the checksum (in gray).</source>
     </message>
     <message>
         <source>Failed to load chat history</source>
-        <translation type="unfinished"></translation>
+        <translation>无法加载聊天记录</translation>
     </message>
     <message>
         <source>Database version (%1) is newer than we currently support (%2). Please upgrade qTox.</source>
-        <translation type="unfinished"></translation>
+        <translation>数据库版本 (%1) 比我们当前支持的 (%2) 更新。请升级 qTox 。</translation>
     </message>
 </context>
 <context>
